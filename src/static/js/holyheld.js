8a6d468b3c9e3b1e115a6f831fd3b8484d2d2d13
Vasilis Fatouros <vfatouros@gmail.com>
2020-10-09 10:57:49 +0100

    Fixed pickle emissions

--- src/static/js/holyheld.js
+++ src/static/js/holyheld.js
@@ -12,10 +12,11 @@ $(function() {
     const HOLY_KNIGHT_ADDR = "0x5D33dE3E540b289f9340D059907ED648c9E7AaDD";
 
     _print(`Staking bonus for long-term holders (multiply by the below APY)`);
-    _print(`3-5  weeks: 1.25x`);
-    _print(`6-8  weeks: 1.5x`);
-    _print(`9-11 weeks: 1.75x`);
-    _print(`12   weeks: 2x\n`);
+    _print(`3-5  weeks: 3.125x`);
+    _print(`6-8  weeks: 3.75x`);
+    _print(`9-11 weeks: 4.375x`);
+    _print(`12   weeks: 5x\n`);
+    _print(`Stake before October 27 to get the full 5x bonus! Program ends approximately January 19.\n`)
 
     await loadChefContractSecondAttempt(App, null, HOLY_KNIGHT_ADDR, HOLY_KNIGHT_ABI, "HOLY", "holytoken", "holyPerBlock", null, "pendingHoly");
   
--- src/static/js/pickle.js
+++ src/static/js/pickle.js
@@ -12,7 +12,7 @@ $(function() {
     const PICKLE_CHEF_ADDR = "0xbD17B1ce622d73bD438b9E658acA5996dc394b0d";
     const rewardTokenTicker = "PICKLE";
     const PICKLE_CHEF = new ethers.Contract(PICKLE_CHEF_ADDR, PICKLE_CHEF_ABI, App.provider);
-    const rewardsPerWeek = await PICKLE_CHEF.picklePerBlock() * await PICKLE_CHEF.BONUS_MULTIPLIER() / 1e18
+    const rewardsPerWeek = await PICKLE_CHEF.picklePerBlock() / 1e18
         * 604800 / 13.5;
 
     await loadChefContractSecondAttempt(App, PICKLE_CHEF, PICKLE_CHEF_ADDR, PICKLE_CHEF_ABI, rewardTokenTicker,
