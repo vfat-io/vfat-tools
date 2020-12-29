708e584a941124f55fc797f119ccffedefac3170
basisq <basisq@basisdollar.fi>
2020-12-29 10:56:39 +0700

    Add BSD pool info

--- src/static/js/bsd.js
+++ src/static/js/bsd.js
@@ -1,59 +1,60 @@
-$(function() {
+$(function () {
     consoleInit();
     start(main);
-  });
+});
 
 async function loadStablePools(App, chef, chefAddress, chefAbi, rewardTokenTicker,
-  rewardTokenFunction, rewardsPerBlockFunction, rewardsPerWeekFixed, pendingRewardsFunction) {
-  const chefContract = chef ?? new ethers.Contract(chefAddress, chefAbi, App.provider);
+                               rewardTokenFunction, rewardsPerBlockFunction, rewardsPerWeekFixed, pendingRewardsFunction) {
+    const chefContract = chef ?? new ethers.Contract(chefAddress, chefAbi, App.provider);
 
-  const poolCount = await chefContract.poolLength() / 1;
+    const poolCount = await chefContract.poolLength() / 1;
 
-  _print(`Found ${poolCount} pools.\n`)
+    _print(`Found ${poolCount} pools.\n`)
 
-  _print(`Showing incentivized pools only.\n`);
+    _print(`Showing incentivized pools only.\n`);
 
-  var tokens = {};
+    var tokens = {};
 
-  const rewardTokenAddress = await chefContract.callStatic[rewardTokenFunction]();
-  const rewardToken = await getToken(App, rewardTokenAddress, chefAddress);
-  const rewardsPerWeek = rewardsPerWeekFixed ??
-    await chefContract.callStatic[rewardsPerBlockFunction]()
-    / 10 ** rewardToken.decimals * 604800 / 13.5
+    const rewardTokenAddress = await chefContract.callStatic[rewardTokenFunction]();
+    const rewardToken = await getToken(App, rewardTokenAddress, chefAddress);
+    const rewardsPerWeek = rewardsPerWeekFixed ??
+        await chefContract.callStatic[rewardsPerBlockFunction]()
+        / 10 ** rewardToken.decimals * 604800 / 13.5
 
-  const poolInfos = await Promise.all([...Array(poolCount).keys()].map(async (x) =>
-    await getPoolInfo(App, chefContract, chefAddress, x, pendingRewardsFunction)));
+    const poolInfos = await Promise.all([...Array(poolCount).keys()].map(async (x) =>
+        await getPoolInfo(App, chefContract, chefAddress, x, pendingRewardsFunction)));
 
-  var tokenAddresses = [].concat.apply([], poolInfos.map(x => x.poolToken.tokens));
-  var prices = await lookUpTokenPrices(tokenAddresses);
+    var tokenAddresses = [].concat.apply([], poolInfos.map(x => x.poolToken.tokens));
+    var prices = await lookUpTokenPrices(tokenAddresses);
 
-  await Promise.all(tokenAddresses.map(async (address) => {
-      tokens[address] = await getToken(App, address, chefAddress);
-  }));
+    await Promise.all(tokenAddresses.map(async (address) => {
+        tokens[address] = await getToken(App, address, chefAddress);
+    }));
 
-  const poolPrices = poolInfos.map(poolInfo => getPoolPrices(tokens, prices, poolInfo.poolToken));
+    const poolPrices = poolInfos.map(poolInfo => getPoolPrices(tokens, prices, poolInfo.poolToken));
 
-  _print("Finished reading smart contracts.\n");
+    _print("Finished reading smart contracts.\n");
 
-  const BSDS_DAI_BALANCER_POOL = "0xdD82e4227BaeC1Fc40a72ef9895f38f2C1Df4F42";
+    const BSDS_DAI_BALANCER_POOL = "0xdD82e4227BaeC1Fc40a72ef9895f38f2C1Df4F42";
 
-  const BSD_USDC_BALANCER_POOL = "0xCDD2bD61D07b8d42843175dd097A4858A8f764e7";
+    const BSD_USDC_BALANCER_POOL = "0xCDD2bD61D07b8d42843175dd097A4858A8f764e7";
 
-  for (const balPoolAddress of [ BSDS_DAI_BALANCER_POOL, BSD_USDC_BALANCER_POOL]) {
-    const balPool = await getToken(App, balPoolAddress, chefAddress);
-    var newTokenAddresses = balPool.tokens.filter(x => tokens[x] == null);
-    for (const address of newTokenAddresses) {
-        tokens[address] = await getToken(App, address, balPoolAddress);
+    for (const balPoolAddress of [BSDS_DAI_BALANCER_POOL, BSD_USDC_BALANCER_POOL]) {
+        const balPool = await getToken(App, balPoolAddress, chefAddress);
+        var newTokenAddresses = balPool.tokens.filter(x => tokens[x] == null);
+        for (const address of newTokenAddresses) {
+            tokens[address] = await getToken(App, address, balPoolAddress);
+        }
+        getPoolPrices(tokens, prices, balPool);
     }
-    getPoolPrices(tokens, prices, balPool);
-  }
 
-  for (i = 0; i < poolCount; i++) {
-    printChefPool(App, chefAbi, chefAddress, prices, tokens, poolInfos[i], i, poolPrices[i],
-      poolCount, rewardsPerWeek, rewardTokenTicker, rewardTokenAddress,
-      pendingRewardsFunction);
-  }
+    for (i = 0; i < poolCount; i++) {
+        printChefPool(App, chefAbi, chefAddress, prices, tokens, poolInfos[i], i, poolPrices[i],
+            poolCount, rewardsPerWeek, rewardTokenTicker, rewardTokenAddress,
+            pendingRewardsFunction);
+    }
 }
+
 const FASS_POOL_ABI = [{
     "inputs": [{"internalType": "uint8", "name": "_pid", "type": "uint8"}, {
         "internalType": "address",
@@ -109,6 +110,8 @@ const FASS_POOL_ABI = [{
     "stateMutability": "view",
     "type": "function"
 },]
+
+
 async function getFaasPoolInfo(app, faasContract, chefAddress, poolIndex) {
     const poolInfo = await faasContract.rewardPoolInfo(poolIndex);
     const poolToken = await getToken(app, chefAddress, chefAddress);
@@ -130,14 +133,22 @@ async function getFaasPoolInfo(app, faasContract, chefAddress, poolIndex) {
     };
 }
 
+function getParameterCaseInsensitive(object, key) {
+    return object[Object.keys(object)
+        .find(k => k.toLowerCase() === key.toLowerCase())
+        ];
+}
+
 async function loadFaasPools(App, faasAddresses) {
     var tokens = {};
+    const BSDS = '0xe7c9c188138f7d70945d420d75f8ca7d8ab9c700';
     for (let {address, poolLength} of faasAddresses) {
 
         const faasContract = new ethers.Contract(address, FASS_POOL_ABI, App.provider);
         const poolInfos = await Promise.all([...Array(poolLength).keys()].map(async (x) =>
             await getFaasPoolInfo(App, faasContract, address, x)));
         var tokenAddresses = [].concat.apply([], poolInfos.map(x => x.poolToken.tokens));
+        tokenAddresses.push(BSDS);
         var prices = await lookUpTokenPrices(tokenAddresses);
         await Promise.all(tokenAddresses.map(async (address) => {
             tokens[address] = await getToken(App, address, address);
@@ -155,7 +166,8 @@ async function loadFaasPools(App, faasAddresses) {
         }
     }
 }
-  async function main() {
+
+async function main() {
     const App = await init_ethers();
 
     _print(`Initialized ${App.YOUR_ADDRESS}\n`);
@@ -167,13 +179,17 @@ async function loadFaasPools(App, faasAddresses) {
 
     await loadStablePools(App, null, STABLES_POOL_ADDR, STABLES_POOL_ABI, rewardTokenTicker,
         "bsd", null, rewardsPerWeek, "pendingBasisDollar");
-      const BSDS_DAI_BALANCER_POOL = "0xdD82e4227BaeC1Fc40a72ef9895f38f2C1Df4F42";
-      const BSD_USDC_BALANCER_POOL = "0x8438d64Da58772E9F7FCeAa1506bA300F935ABBd";
-      _print("Reading share bank smart contracts...\n");
-      const tokens = await loadFaasPools(App,
-          [{address: BSDS_DAI_BALANCER_POOL, poolLength: 1}, {
-              address: BSD_USDC_BALANCER_POOL,
-              poolLength: 1
-          }]);
+    const BSD_DAI_BALANCER_POOL = "0xc1b6296e55b6cA1882a9cefD72Ac246ACdE91414";
+    const BSD_USDC_BALANCER_POOL = "0xCDD2bD61D07b8d42843175dd097A4858A8f764e7";
+    const BSDS_USDC_BALANCER_POOL = "0x8438d64Da58772E9F7FCeAa1506bA300F935ABBd";
+    const BSDS_DAI_BALANCER_POOL = "0xdD82e4227BaeC1Fc40a72ef9895f38f2C1Df4F42";
+    _print("Reading share bank smart contracts...\n");
+    const tokens = await loadFaasPools(App,
+        [
+            {address: BSD_USDC_BALANCER_POOL, poolLength: 1},
+            {address: BSD_DAI_BALANCER_POOL, poolLength: 1},
+            {address: BSDS_DAI_BALANCER_POOL, poolLength: 1},
+            {address: BSDS_USDC_BALANCER_POOL, poolLength: 1},
+        ]);
     hideLoading();
-  }
+}
