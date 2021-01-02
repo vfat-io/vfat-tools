32673a51ea280bdf96f6c0382be7f06961ea2cc3
vfat <info@vfat.tools>
2021-01-02 00:23:18 +0000

    Fixed DSD epochs

--- src/static/js/ethers_helper.js
+++ src/static/js/ethers_helper.js
@@ -1574,3 +1574,57 @@ async function calculateDollarAPR(DAO, parameters, twap, dollarPrice, uniPrices,
 
     _print(`LP  APR: Day ${lpReturn.toFixed(2)}% Week ${(lpReturn * 7).toFixed(2)}% Year ${(lpReturn * 365).toFixed(2)}%`)  
 }
+
+async function loadDollar(contractInfo, calcPrice) {
+  const params = contractInfo.Parameters;
+  
+  const App = await init_ethers();
+
+  _print(`Initialized ${App.YOUR_ADDRESS}\n`);
+  _print("Reading smart contracts...\n");
+
+  const DAO = new ethers.Contract(contractInfo.DAO.address, contractInfo.DAO.abi, App.provider);
+  const DOLLAR = new ethers.Contract(contractInfo.Dollar.address, ERC20_ABI, App.provider);
+  var prices = {};
+  var tokens = {};
+
+  const [epoch, uniPrices, totalBonded] = await loadDAO(App, DAO, DOLLAR, contractInfo.UniswapLP.address,
+      contractInfo.LPIncentivizationPool.address, tokens, prices, params.DaoLockupPeriods);
+
+  const LP = new ethers.Contract(contractInfo.LPIncentivizationPool.address,
+      contractInfo.LPIncentivizationPool.abi, App.provider);
+  await loadEmptySetLP(App, LP, contractInfo.UniswapLP.address, 
+      contractInfo.LPIncentivizationPool.ticker, params.PoolLockupPeriods, 
+      epoch, contractInfo.Dollar.ticker, uniPrices);
+  
+  const dollarPrice = getParameterCaseInsensitive(prices, DOLLAR.address).usd;
+
+  if (epoch < params.BootstrappingPeriod) { //bootstrapping
+      const twap = params.BootstrappingPrice;      
+      await calculateDollarAPR(DAO, params, twap, dollarPrice, uniPrices, totalBonded, calcPrice);
+  }
+  else {
+      const resp = await fetch('https://api.vfat.tools/twap/' + contractInfo.UniswapLP.address);
+      const text = await resp.text();
+      const array = text.split("\n");
+      if (array.length > 0) {
+          const [oldPrice0, , oldTimestamp] = array[array.length - 2].split(' ') //last line is blank
+          const [price0, , timestamp] = await getCurrentPriceAndTimestamp(App, contractInfo.UniswapLP.address);
+          const twap = await calculateTwap(oldPrice0, oldTimestamp, price0, timestamp, 12);
+          _print(`TWAP: ${twap}\n`);
+
+          if (twap > params.GrowCutoff ?? 1) {
+              await calculateDollarAPR(DAO, contractInfo.Parameters, twap, dollarPrice, uniPrices, calcPrice);
+          }
+          else {
+              _print(`DAO APR: Day 0% Week 0% Year 0%`)
+              _print(`LP APR: Day 0% Week 0% Year 0%`)
+          }        
+      }
+  }
+  _print(`\nDAO Unbonds`)
+  await printDaoUnbonds(App.provider, DAO, epoch + 1, params.DaoLockupPeriods, params.EpochPeriod);
+  _print(`\LP Unbonds`)
+  await printLPUnbonds(App.provider, LP, epoch + 1, params.PoolLockupPeriods, params.EpochPeriod);
+  hideLoading();  
+}
