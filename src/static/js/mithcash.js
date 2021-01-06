62c3ed4bfb1dae83a68d162e4a8aa639e89f9d65
vfat <info@vfat.tools>
2021-01-06 22:09:56 +0000

    streamlined basis fork code

--- src/static/js/mithcash.js
+++ src/static/js/mithcash.js
@@ -3,144 +3,6 @@ $(function() {
     start(main);
 });
 
-async function loadPool(App, tokens, prices, stakingAbi, stakingAddress,
-        rewardTokenFunction, stakeTokenFunction) {
-    const STAKING_POOL = new ethers.Contract(stakingAddress, stakingAbi, App.provider);
-
-    const stakeTokenAddress = await STAKING_POOL.callStatic[stakeTokenFunction]();
-
-    const rewardTokenAddress = await STAKING_POOL.callStatic[rewardTokenFunction]();
-
-    var stakeToken = await getToken(App, stakeTokenAddress, stakingAddress);
-
-    if (stakeTokenAddress.toLowerCase() === rewardTokenAddress.toLowerCase()) {
-        stakeToken.staked = await STAKING_POOL.totalSupply() / 10 ** stakeToken.decimals;
-    }
-
-    var newPriceAddresses = stakeToken.tokens.filter(x =>
-        !getParameterCaseInsensitive(prices, x));
-    var newPrices = await lookUpTokenPrices(newPriceAddresses);
-    for (const key in newPrices) {
-        if (newPrices[key])
-            prices[key] = newPrices[key];
-    }
-    if (stakeTokenFunction === "SUSD") {
-        prices[stakeTokenAddress] = { usd : 1 }; //...
-    }
-    var newTokenAddresses = stakeToken.tokens.filter(x =>
-        !getParameterCaseInsensitive(tokens,x));
-    for (const address of newTokenAddresses) {
-        tokens[address] = await getToken(App, address, stakingAddress);
-    }
-    if (!getParameterCaseInsensitive(tokens, rewardTokenAddress)) {
-        tokens[rewardTokenAddress] = await getToken(App, rewardTokenAddress, stakingAddress);
-    }
-    const rewardToken = getParameterCaseInsensitive(tokens, rewardTokenAddress);
-
-    const rewardTokenTicker = rewardToken.symbol;
-
-    const poolPrices = getPoolPrices(tokens, prices, stakeToken);
-
-    const stakingTokenTicker = poolPrices.stakingTokenTicker;
-
-    const stakeTokenPrice =
-        prices[stakeTokenAddress]?.usd ?? getParameterCaseInsensitive(prices, stakeTokenAddress)?.usd;
-    const rewardTokenPrice = getParameterCaseInsensitive(prices, rewardTokenAddress)?.usd;
-
-    // Find out reward rate
-    const weeklyRewards = await get_synth_weekly_rewards(STAKING_POOL);
-
-    const usdPerWeek = weeklyRewards * rewardTokenPrice;
-
-    const staked_tvl = poolPrices.staked_tvl;
-
-    const userStaked = await STAKING_POOL.balanceOf(App.YOUR_ADDRESS) / 10 ** stakeToken.decimals;
-
-    const userUnstaked = stakeToken.unstaked;
-
-    const earned = await STAKING_POOL.earned(App.YOUR_ADDRESS) / 10 ** rewardToken.decimals;
-
-    poolPrices.print_price();
-    _print(`${rewardTokenTicker} Per Week: ${weeklyRewards.toFixed(2)} ($${formatMoney(usdPerWeek)})`);
-    const weeklyAPY = usdPerWeek / staked_tvl * 100;
-    const dailyAPY = weeklyAPY / 7;
-    const yearlyAPY = weeklyAPY * 52;
-    _print(`APY: Day ${dailyAPY.toFixed(2)}% Week ${weeklyAPY.toFixed(2)}% Year ${yearlyAPY.toFixed(2)}%`);
-    const userStakedUsd = userStaked * stakeTokenPrice;
-    const userStakedPct = userStakedUsd / staked_tvl * 100;
-    _print(`You are staking ${userStaked.toFixed(6)} ${stakingTokenTicker} ` +
-           `$${formatMoney(userStakedUsd)} (${userStakedPct.toFixed(2)}% of the pool).`);
-    if (userStaked > 0) {
-        const userWeeklyRewards = userStakedPct * weeklyRewards / 100;
-        const userDailyRewards = userWeeklyRewards / 7;
-        const userYearlyRewards = userWeeklyRewards * 52;
-        _print(`Estimated ${rewardTokenTicker} earnings:`
-            + ` Day ${userDailyRewards.toFixed(2)} ($${formatMoney(userDailyRewards*rewardTokenPrice)})`
-            + ` Week ${userWeeklyRewards.toFixed(2)} ($${formatMoney(userWeeklyRewards*rewardTokenPrice)})`
-            + ` Year ${userYearlyRewards.toFixed(2)} ($${formatMoney(userYearlyRewards*rewardTokenPrice)})`);
-    }
-    const approveTENDAndStake = async function() {
-      return rewardsContract_stake(stakeTokenAddress, stakingAddress, App)
-    }
-    const unstake = async function() {
-      return rewardsContract_unstake(stakingAddress, App)
-    }
-    const claim = async function() {
-      return rewardsContract_claim(stakingAddress, App)
-    }
-    const exit = async function() {
-      return rewardsContract_exit(stakingAddress, App)
-    }
-    const revoke = async function() {
-      return rewardsContract_resetApprove(stakeTokenAddress, stakingAddress, App)
-    }
-    if (stakeTokenFunction !== "mith") {
-        _print_link(`Stake ${userUnstaked.toFixed(6)} ${stakingTokenTicker}`, approveTENDAndStake)
-    }
-    _print_link(`Unstake ${userStaked.toFixed(6)} ${stakingTokenTicker}`, unstake)
-    _print_link(`Claim ${earned.toFixed(6)} ${rewardTokenTicker}`, claim)
-    _print_link(`Revoke (set approval to 0)`, revoke)
-    _print_link(`Exit`, exit)
-    _print(`\n`);
-
-    return {
-        staked_tvl: poolPrices.staked_tvl,
-    }
-}
-
-async function loadBoardroom(App, tokens, prices) {
-    const BOARDROOM_ADDRESS = "0xb35f89160d1Dc47B6EAC1986D7821505c327AE09";
-    const BOARDROOM = new ethers.Contract(BOARDROOM_ADDRESS, BOARDROOM_ABI, App.provider);
-    const share = await BOARDROOM.share();
-    const SHARE = new ethers.Contract(share, ERC20_ABI, App.provider);
-    const userUnstaked = await SHARE.balanceOf(App.YOUR_ADDRESS) / 1e18;
-    const sharePrice = getParameterCaseInsensitive(prices, share)?.usd;
-    const userStaked = await BOARDROOM.balanceOf(App.YOUR_ADDRESS) / 1e18;
-    const userStakedUsd = userStaked * sharePrice;
-    const totalStaked = await BOARDROOM.totalSupply() / 1e18;
-    const totalStakedUsd = totalStaked * sharePrice;
-    const userPct = userStaked / totalStaked * 100;
-    const earned = await BOARDROOM.earned(App.YOUR_ADDRESS) / 1e18;
-    _print(`Boardroom`);
-    _print(`There is a total ${totalStaked.toFixed(2)} MIS ($${formatMoney(totalStakedUsd)}) staked in the Boardroom.`)
-    _print(`You are staking ${userStaked} MIS ($${formatMoney(userStakedUsd)}), ${userPct.toFixed(2)}% of the pool.`);
-
-    const approveTENDAndStake = async () => rewardsContract_stake(share, BOARDROOM_ADDRESS, App);
-    const unstake = async () => rewardsContract_unstake(BOARDROOM_ADDRESS, App);
-    const claim = async () => rewardsContract_claim(BOARDROOM_ADDRESS, App);
-    const exit = async () =>  rewardsContract_exit(BOARDROOM_ADDRESS, App);
-    const revoke = async () => rewardsContract_resetApprove(share, BOARDROOM_ADDRESS, App);
-
-    _print_link(`Stake ${userUnstaked.toFixed(2)} MIS`, approveTENDAndStake)
-    _print_link(`Unstake ${userStaked.toFixed(2)} MIS`, unstake)
-    _print_link(`Claim ${earned.toFixed(2)} MIC`, claim)
-    _print_link(`Revoke (set approval to 0)`, revoke)
-    _print_link(`Exit`, exit)
-    _print(`\n`);
-
-    return totalStakedUsd;
-}
-
 async function main() {
     const CONTRACTS = [
         { address: "0x14E33e1D6Cc4D83D7476492C0A52b3d4F869d892", abi : MITH_USDTMIC_ABI, rewardToken: "mithShare", stakeToken: "lpt"},
@@ -176,15 +38,20 @@ async function main() {
 
     for (const c of CONTRACTS) {
         try {
-            const { staked_tvl } = await loadPool(App, tokens, prices, c.abi, c.address, c.rewardToken, c.stakeToken);
+            const { staked_tvl } = await loadSynthetixPool(App, tokens, prices, c.abi, c.address, c.rewardToken, c.stakeToken);
             totalStaked += staked_tvl;
         }
         catch (ex) {
             console.error(ex);
         }
     }
-    totalStaked += await loadBoardroom(App, tokens, prices);
-
+    const BOARDROOM_ADDRESS = "0xb35f89160d1Dc47B6EAC1986D7821505c327AE09";
+    const ORACLE_ADDRESS = "0x31f6DA78Fd83f55Eb3823364e4a103Cf9036b9FC";
+    const MIC_USDT_SLP_ADDRESS = "0xc9cb53b48a2f3a9e75982685644c1870f1405ccb";
+    const MIC_ADDRESS = "0x368b3a58b5f49392e5c9e4c998cb0bb966752e51"
+    const boardroomTvl = await loadBoardroom(App, prices, BOARDROOM_ADDRESS, ORACLE_ADDRESS, MIC_USDT_SLP_ADDRESS, MIC_ADDRESS,
+        "MIS", "MIC", 1, Infinity, 2, 1, 12);
+    totalStaked += boardroomTvl.stakedTvl;
     _print_bold(`Total staked: $${formatMoney(totalStaked)}`)
 
     hideLoading();
--- src/static/js/xusd.js
+++ src/static/js/xusd.js
@@ -3,91 +3,6 @@ $(function () {
     start(main);
 });
 
-async function loadPool(App, tokens, prices, stakingAddress) {
-    console.log("Loading Pool...")
-    const STAKING_POOL = new ethers.Contract(stakingAddress, XUSD_STAKING_POOL_ABI, App.provider);
-
-    const stakeTokenAddress = await STAKING_POOL.stakingToken();
-
-    const rewardTokenAddress = await STAKING_POOL.rewardsToken();
-
-    var stakeToken = await getToken(App, stakeTokenAddress, stakingAddress);
-
-    if (stakeTokenAddress.toLowerCase() == rewardTokenAddress.toLowerCase()) {
-        stakeToken.staked = await STAKING_POOL.totalSupply() / 10 ** stakeToken.decimals;
-    }
-
-    var newPriceAddresses = stakeToken.tokens.filter(x => prices[x] == null);
-    var newPrices = await lookUpTokenPrices(newPriceAddresses);
-    for (const key in newPrices) {
-        prices[key] = newPrices[key];
-    }
-    var newTokenAddresses = stakeToken.tokens.filter(x => tokens[x] == null);
-    for (const address of newTokenAddresses) {
-        tokens[address] = await getToken(App, address, stakingAddress);
-    }
-    const rewardToken = getParameterCaseInsensitive(tokens, rewardTokenAddress);
-
-    const rewardTokenTicker = rewardToken.symbol;
-
-    const poolPrices = getPoolPrices(tokens, prices, stakeToken);
-
-    const stakingTokenTicker = poolPrices.stakingTokenTicker;
-
-    const stakeTokenPrice = getParameterCaseInsensitive(prices, stakeTokenAddress).usd;
-    const rewardTokenPrice = getParameterCaseInsensitive(prices, rewardTokenAddress).usd;
-
-    // Find out reward rate
-    const weeklyRewards = await get_synth_weekly_rewards(STAKING_POOL);
-
-    const usdPerWeek = weeklyRewards * rewardTokenPrice;
-
-    const staked_tvl = poolPrices.staked_tvl;
-
-    const userStaked = await STAKING_POOL.balanceOf(App.YOUR_ADDRESS) / 10 ** stakeToken.decimals;
-
-    const userUnstaked = stakeToken.unstaked;
-
-    const earned = await STAKING_POOL.earned(App.YOUR_ADDRESS) / 10 ** rewardToken.decimals;
-
-    poolPrices.print_price();
-    _print(`${rewardTokenTicker} Per Week: ${weeklyRewards.toFixed(2)} ($${formatMoney(usdPerWeek)})`);
-    const weeklyAPY = usdPerWeek / staked_tvl * 100;
-    const dailyAPY = weeklyAPY / 7;
-    const yearlyAPY = weeklyAPY * 52;
-    _print(`APY: Day ${dailyAPY.toFixed(2)}% Week ${weeklyAPY.toFixed(2)}% Year ${yearlyAPY.toFixed(2)}%`);
-    const userStakedUsd = userStaked * stakeTokenPrice;
-    const userStakedPct = userStakedUsd / staked_tvl * 100;
-    _print(`You are staking ${userStaked.toFixed(6)} ${stakingTokenTicker} ` +
-        `$${formatMoney(userStakedUsd)} (${userStakedPct.toFixed(2)}% of the pool).`);
-    if (userStaked > 0) {
-        const userWeeklyRewards = userStakedPct * weeklyRewards / 100;
-        const userDailyRewards = userWeeklyRewards / 7;
-        const userYearlyRewards = userWeeklyRewards * 52;
-        _print(`Estimated ${rewardTokenTicker} earnings:`
-            + ` Day ${userDailyRewards.toFixed(2)} ($${formatMoney(userDailyRewards * rewardTokenPrice)})`
-            + ` Week ${userWeeklyRewards.toFixed(2)} ($${formatMoney(userWeeklyRewards * rewardTokenPrice)})`
-            + ` Year ${userYearlyRewards.toFixed(2)} ($${formatMoney(userYearlyRewards * rewardTokenPrice)})`);
-    }
-    const approveTENDAndStake = async function () {
-        return rewardsContract_stake(stakeTokenAddress, stakingAddress, App)
-    }
-    const unstake = async function () {
-        return rewardsContract_unstake(stakingAddress, App)
-    }
-    const claim = async function () {
-        return rewardsContract_claim(stakingAddress, App)
-    }
-    const exit = async function () {
-        return rewardsContract_exit(stakingAddress, App)
-    }
-    _print_link(`Stake ${userUnstaked.toFixed(6)} ${stakingTokenTicker}`, approveTENDAndStake)
-    _print_link(`Unstake ${userStaked.toFixed(6)} ${stakingTokenTicker}`, unstake)
-    _print_link(`Claim ${earned.toFixed(6)} ${rewardTokenTicker}`, claim)
-    _print_link(`Exit`, exit)
-    _print(`\n`);
-}
-
 async function main() {
 
     const Contracts = {
@@ -115,10 +30,14 @@ async function main() {
 
     var tokens = {};
     var prices = {};
+    var totalStaked = 0;
 
     for (const [, address] of Object.entries(Contracts.Staking)) {
-        await loadPool(App, tokens, prices, address);
+        const { staked_tvl } = await loadSynthetixPool(App, tokens, prices, XUSD_STAKING_POOL_ABI, address, 
+            "rewardsToken", "stakingToken");
+        totalStaked += staked_tvl;
     }
+    _print_bold(`Total staked: $${formatMoney(totalStaked)}`)
 
     hideLoading();
 }
