f2c584f38638872cafd073fa3f833011f19b72a9
Vasilis Fatouros <vfatouros@gmail.com>
2021-01-30 23:27:25 +0000

    Added 1inch v1.1 pools

--- src/static/js/1inch.js
+++ src/static/js/1inch.js
@@ -4,13 +4,19 @@ $(function() {
 });
 
 const Pools = [ 
-  {
-    address: "0x18D410f651289BB978Fc32F90D2d7E608F4f4560",
+  "0x18D410f651289BB978Fc32F90D2d7E608F4f4560", //ETH-OPIUM farm
+  "0x9070832CF729A5150BB26825c2927e7D343EabD9", //ETH-1INCH farm  
+  "0xA83fCeA9229C7f1e02Acb46ABe8D6889259339e8", //ETH-USDT farm  
+  "0x4dab1Ba9609C1546A0A69a76F00eD935b0b9C45e", //ETH-DAI farm  
+  "0x0DA1b305d7101359434d71eCEAab71E1fF5437e6", //ETH-USDC farm  
+  "0x98484d4259A70B73af58180521f2eB71a3F00Ae6", //ETH-WBTC farm
+].map(a => { 
+  return {
+    address: a,
     abi: _1INCH_FARMING_ABI,
     stakeTokenFunction: "mooniswap",
     rewardTokenFunction: "gift"
-  }
-]
+  }; })
 
 async function main() {
 
