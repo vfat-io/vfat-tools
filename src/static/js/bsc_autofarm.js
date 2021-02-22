f4ce6b5910c3aaac8d5db7e851a6beaf5b1aed67
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-02-22 17:46:03 +0200

    Added autofarm

---
+++ src/static/js/bsc_autofarm.js
@@ -0,0 +1,115 @@
+
+$(function() {
+    consoleInit();
+    start(main);
+  });
+
+const AUTOFARM_CHEF_ABI = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"EmergencyWithdraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdraw","type":"event"},{"inputs":[],"name":"AUTO","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"AUTOMaxSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"AUTOPerBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"AUTOv2","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_allocPoint","type":"uint256"},{"internalType":"contract IERC20","name":"_want","type":"address"},{"internalType":"bool","name":"_withUpdate","type":"bool"},{"internalType":"address","name":"_strat","type":"address"}],"name":"add","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"burnAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_wantAmt","type":"uint256"}],"name":"deposit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"emergencyWithdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_from","type":"uint256"},{"internalType":"uint256","name":"_to","type":"uint256"}],"name":"getMultiplier","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_token","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"inCaseTokensGetStuck","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"massUpdatePools","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_inputAmt","type":"uint256"}],"name":"migrateToAUTOv2","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ownerAUTOReward","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"address","name":"_user","type":"address"}],"name":"pendingAUTO","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"poolInfo","outputs":[{"internalType":"contract IERC20","name":"want","type":"address"},{"internalType":"uint256","name":"allocPoint","type":"uint256"},{"internalType":"uint256","name":"lastRewardBlock","type":"uint256"},{"internalType":"uint256","name":"accAUTOPerShare","type":"uint256"},{"internalType":"address","name":"strat","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"poolLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_allocPoint","type":"uint256"},{"internalType":"bool","name":"_withUpdate","type":"bool"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"address","name":"_user","type":"address"}],"name":"stakedWantTokens","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"startBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalAllocPoint","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"updatePool","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"address","name":"","type":"address"}],"name":"userInfo","outputs":[{"internalType":"uint256","name":"shares","type":"uint256"},{"internalType":"uint256","name":"rewardDebt","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_wantAmt","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"withdrawAll","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
+async function main() {  
+    const App = await init_ethers();
+  
+    _print(`Initialized ${App.YOUR_ADDRESS}\n`);
+    _print("Reading smart contracts...\n");
+
+   const AUTOFARM_CHEF_ADDR = "0x0895196562C7868C5Be92459FaE7f877ED450452";
+   const rewardTokenTicker = "AUTO";
+   const AUTOFARM_CHEF = new ethers.Contract(AUTOFARM_CHEF_ADDR, AUTOFARM_CHEF_ABI, App.provider);
+
+   const rewardsPerWeek = await AUTOFARM_CHEF.AUTOPerBlock() /1e18
+        * 604800 / 3;
+
+    const tokens = {};
+    const prices = await getBscPrices();
+
+    await loadBscAUTOChefContract(App, tokens, prices, AUTOFARM_CHEF, AUTOFARM_CHEF_ADDR, AUTOFARM_CHEF_ABI, rewardTokenTicker,
+        "AUTO", null, rewardsPerWeek, "pendingAUTO", [1]);
+
+    hideLoading();  
+  }
+
+  async function loadBscAUTOChefContract(App, tokens, prices, chef, chefAddress, chefAbi, rewardTokenTicker,
+    rewardTokenFunction, rewardsPerBlockFunction, rewardsPerWeekFixed, pendingRewardsFunction,
+    deathPoolIndices) {
+    const chefContract = chef ?? new ethers.Contract(chefAddress, chefAbi, App.provider);
+  
+    const poolCount = parseInt(await chefContract.poolLength(), 10);
+    const totalAllocPoints = await chefContract.totalAllocPoint();
+  
+    _print(`Found ${poolCount} pools.\n`)
+  
+    _print(`Showing incentivized pools only.\n`);
+  
+    var tokens = {};
+  
+    const rewardTokenAddress = await chefContract.callStatic[rewardTokenFunction]();
+    const rewardToken = await getBscToken(App, rewardTokenAddress, chefAddress);
+    const rewardsPerWeek = rewardsPerWeekFixed ?? 
+      await chefContract.callStatic[rewardsPerBlockFunction]() 
+      / 10 ** rewardToken.decimals * 604800 / 3
+  
+    const poolInfos = await Promise.all([...Array(poolCount).keys()].map(async (x) =>
+      await getBscAUTOPoolInfo(App, chefContract, chefAddress, x, pendingRewardsFunction)));
+  
+    var tokenAddresses = [].concat.apply([], poolInfos.filter(x => x.poolToken).map(x => x.poolToken.tokens));
+  
+    await Promise.all(tokenAddresses.map(async (address) => {
+        tokens[address] = await getBscToken(App, address, chefAddress);
+    }));
+  
+    if (deathPoolIndices) {   //load prices for the deathpool assets
+      deathPoolIndices.map(i => poolInfos[i])
+                       .map(poolInfo => 
+        poolInfo.poolToken ? getPoolPrices(tokens, prices, poolInfo.poolToken) : undefined);
+    }
+  
+    const poolPrices = poolInfos.map(poolInfo => poolInfo.poolToken ? getPoolPrices(tokens, prices, poolInfo.poolToken) : undefined);
+  
+  
+    _print("Finished reading smart contracts.\n");
+      
+    for (i = 0; i < poolCount; i++) {
+      if (poolPrices[i]) {
+        printChefPool(App, chefAbi, chefAddress, prices, tokens, poolInfos[i], i, poolPrices[i],
+          totalAllocPoints, rewardsPerWeek, rewardTokenTicker, rewardTokenAddress,
+          pendingRewardsFunction);
+      }
+    }
+  }
+
+  async function getBscAUTOPoolInfo(app, chefContract, chefAddress, poolIndex, pendingRewardsFunction) {  
+    const poolInfo = await chefContract.poolInfo(poolIndex);
+    if (poolInfo.allocPoint == 0 || poolInfo.accAUTOPerShare == 0) {
+      return {
+        address: poolInfo.want,
+        allocPoints: poolInfo.allocPoint ?? 1,
+        poolToken: null,
+        userStaked : 0,
+        pendingRewardTokens : 0,
+        stakedToken : null,
+        userLPStaked : 0,
+        lastRewardBlock : poolInfo.lastRewardBlock
+      };
+    }
+    const poolToken = await getBscToken(app, poolInfo.want, chefAddress);
+    const userInfo = await chefContract.userInfo(poolIndex, app.YOUR_ADDRESS);
+    const pendingRewardTokens = await chefContract.callStatic[pendingRewardsFunction](poolIndex, app.YOUR_ADDRESS);
+    const staked = userInfo.amount / 10 ** poolToken.decimals;
+    var stakedToken;
+    var userLPStaked;
+    if (poolInfo.stakedHoldableToken != null && 
+      poolInfo.stakedHoldableToken != "0x0000000000000000000000000000000000000000") {
+      stakedToken = await getBscToken(app, poolInfo.stakedHoldableToken, chefAddress);
+      userLPStaked = userInfo.stakedLPAmount / 10 ** poolToken.decimals
+    }
+    return {
+        address: poolInfo.want,
+        allocPoints: poolInfo.allocPoint ?? 1,
+        poolToken: poolToken,
+        userStaked : staked,
+        pendingRewardTokens : pendingRewardTokens / 10 ** 18,
+        stakedToken : stakedToken,
+        userLPStaked : userLPStaked,
+        lastRewardBlock : poolInfo.lastRewardBlock
+    };
+  }
