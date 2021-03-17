3d7864e44286fb811083ac57bc880b8b6f7e367e
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-03-17 13:45:12 +0200

    coverprotocol progress

--- src/static/js/coverprotocol.js
+++ src/static/js/coverprotocol.js
@@ -5,7 +5,7 @@ $(function() {
 });
 
 const COVER_CONTRACT_ABI = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"address","name":"lpToken","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"address","name":"lpToken","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdraw","type":"event"},{"inputs":[{"internalType":"address","name":"_lpToken","type":"address"},{"internalType":"address","name":"_bonusTokenAddr","type":"address"},{"internalType":"uint48","name":"_startTime","type":"uint48"},{"internalType":"uint256","name":"_weeklyRewards","type":"uint256"},{"internalType":"uint256","name":"_transferAmount","type":"uint256"}],"name":"addBonus","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address[]","name":"_lpTokens","type":"address[]"},{"internalType":"address[]","name":"_bonusTokenAddrs","type":"address[]"},{"internalType":"address[]","name":"_authorizers","type":"address[]"}],"name":"addPoolsAndAllowBonus","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address[]","name":"_lpTokens","type":"address[]"}],"name":"claimRewardsForPools","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_token","type":"address"},{"internalType":"address","name":"_lpToken","type":"address"},{"internalType":"uint256","name":"_poolBonusId","type":"uint256"}],"name":"collectDust","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_lpToken","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"deposit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address[]","name":"_lpTokens","type":"address[]"}],"name":"emergencyWithdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_lpToken","type":"address"},{"internalType":"uint256","name":"_poolBonusId","type":"uint256"},{"internalType":"address","name":"_bonusTokenAddr","type":"address"},{"internalType":"uint256","name":"_transferAmount","type":"uint256"}],"name":"extendBonus","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_lpToken","type":"address"},{"internalType":"address","name":"_bonusTokenAddr","type":"address"}],"name":"getAuthorizers","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_lpToken","type":"address"}],"name":"getPool","outputs":[{"components":[{"components":[{"internalType":"address","name":"bonusTokenAddr","type":"address"},{"internalType":"uint48","name":"startTime","type":"uint48"},{"internalType":"uint48","name":"endTime","type":"uint48"},{"internalType":"uint256","name":"weeklyRewards","type":"uint256"},{"internalType":"uint256","name":"accRewardsPerToken","type":"uint256"},{"internalType":"uint256","name":"remBonus","type":"uint256"}],"internalType":"struct IBonusRewards.Bonus[]","name":"bonuses","type":"tuple[]"},{"internalType":"uint256","name":"lastUpdatedAt","type":"uint256"}],"internalType":"struct IBonusRewards.Pool","name":"","type":"tuple"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getPoolList","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getResponders","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_lpToken","type":"address"},{"internalType":"address","name":"_account","type":"address"}],"name":"getUser","outputs":[{"components":[{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256[]","name":"rewardsWriteoffs","type":"uint256[]"}],"internalType":"struct IBonusRewards.User","name":"","type":"tuple"},{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"paused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bool","name":"_paused","type":"bool"}],"name":"setPaused","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address[]","name":"_responders","type":"address[]"}],"name":"setResponders","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_lpToken","type":"address"},{"internalType":"address","name":"_bonusTokenAddr","type":"address"},{"internalType":"uint256","name":"_weeklyRewards","type":"uint256"},{"internalType":"uint48","name":"_startTime","type":"uint48"}],"name":"updateBonus","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_lpToken","type":"address"},{"internalType":"address","name":"_user","type":"address"}],"name":"viewRewards","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_lpToken","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]
-//const provider = new ethers.providers.JsonRpcProvider("https://eth-mainnet.alchemyapi.io/v2/nWdyYF9wjUbn7KIwVnh3r571aIq0A6kq");
+
 async function main() {
 
   const App = await init_ethers();
@@ -27,29 +27,110 @@ async function main() {
       let pool = await COVER_CONTRACT.getPool(poolAddress);
       let rewards = []
       let pendingRewards = await COVER_CONTRACT.viewRewards(poolAddress, App.YOUR_ADDRESS);
-      for(reward of pool.bonuses){
+      for(let i = 0; i < pool.bonuses.length; i++){
           rewards.push({
-              rewardToken: reward.bonusTokenAddr,
-              weeklyRewards: reward.weeklyRewards
+              rewardToken: pool.bonuses[i].bonusTokenAddr,
+              weeklyRewards: pool.bonuses[i].weeklyRewards,
+              pendingRewards : pendingRewards[i]
           });
       }
       pools.push({
-          stakedToken: poolAddress,
-          rewards : rewards,
-          pendingRewards : pendingRewards
+          abi : COVER_CONTRACT_ABI,
+          address : COVER_CONTRACT_ADDR,
+          stakedToken : poolAddress,
+          rewards : rewards
       });
   }
 
 
-  /*let p = await loadMultipleSynthetixPools(App, tokens, prices, Pools)
+  let p = await loadCoverprotocolContracts(App, tokens, prices, pools)
   _print_bold(`Total staked: $${formatMoney(p.staked_tvl)}`);
   if (p.totalUserStaked > 0) {
     _print(`You are staking a total of $${formatMoney(p.totalUserStaked)} at an APR of ${(p.totalAPR * 100).toFixed(2)}%\n`);
   }
 
-  _print_bold(`\n*** Participant Rewards ***`)
-  _print(`By validating your profile you will earn 1 UBI/hr.`)
-  const dailyUBI = getParameterCaseInsensitive(prices, "0xDd1Ad9A21Ce722C151A836373baBe42c868cE9a4").usd * 24
-  _print(`Estimated earnings: Day $${formatMoney(dailyUBI)}, Week $${formatMoney(dailyUBI*7)}, Year $${formatMoney(dailyUBI*365)}`)*/
   hideLoading();
+}
+
+async function loadCoverprotocolContracts(App, tokens, prices, pools) {
+  let totalStaked  = 0, totalUserStaked = 0, individualAPRs = [];
+  const infos = await Promise.all(pools.map(p => 
+    loadCoverprotocolPoolInfo(App, tokens, prices, p.abi, p.address, p.stakedToken,
+      p.rewards)));
+  for (const i of infos) {
+    let p = await printSynthetixPool(App, i);
+    totalStaked += p.staked_tvl || 0;
+    totalUserStaked += p.userStaked || 0;
+    if (p.userStaked > 0) {
+      individualAPRs.push(p.userStaked * p.apr / 100);
+    }
+  }
+  let totalAPR = totalUserStaked == 0 ? 0 : individualAPRs.reduce((x,y)=>x+y, 0) / totalUserStaked;
+  return { staked_tvl : totalStaked, totalUserStaked, totalAPR };
+}
+
+async function loadCoverprotocolPoolInfo(App, tokens, prices, stakingAbi, stakingAddress, stakeTokenAddress,
+  rewards) {
+    const STAKING_CONTRACT = new ethers.Contract(stakingAddress, stakingAbi, App.YOUR_ADDRESS);
+
+    const rewardTokens = rewards.map(r => {
+      let rewardTokenPrice = getParameterCaseInsensitive(prices, r.rewardToken)?.usd;
+      if (!getParameterCaseInsensitive(tokens, r.rewardToken)) {
+        tokens[r.rewardToken] = await getToken(App, rewardTokenAddress, stakingAddress);
+      }
+      let rewardToken = getParameterCaseInsensitive(tokens, r.rewardToken);
+      let rewardTokenTicker = rewardToken.symbol;
+      let userInfos = await STAKING_CONTRACT.getUser(stakeTokenAddress, App.YOUR_ADDRESS);
+      let earned = r.pendingRewards / 10 ** rewardToken.decimals;
+      return {
+        address : r.rewardToken,
+        weeklyRewards : r.weeklyRewards,
+        usdPerWeek : r.weeklyRewards * rewardTokenPrice,
+        rewardToken : rewardToken,
+        rewardTokenTicker : rewardTokenTicker,
+        usersBalance : userInfos[0].amount,
+        earned : earned
+      }
+    });
+
+    let stakeToken = await getToken(App, stakeTokenAddress, stakingAddress);
+
+    let newPriceAddresses = stakeToken.tokens.filter(x =>
+      !getParameterCaseInsensitive(prices, x));
+    let newPrices = await lookUpTokenPrices(newPriceAddresses);
+    for (const key in newPrices) {
+      if (newPrices[key]?.usd)
+          prices[key] = newPrices[key];
+    }
+    let newTokenAddresses = stakeToken.tokens.filter(x =>
+      !getParameterCaseInsensitive(tokens,x));
+    for (const address of newTokenAddresses) {
+        tokens[address] = await getToken(App, address, stakingAddress);
+    }
+
+    const poolPrices = getPoolPrices(tokens, prices, stakeToken);
+
+    const stakeTokenTicker = poolPrices.stakeTokenTicker;
+
+    const stakeTokenPrice =
+        prices[stakeTokenAddress]?.usd ?? getParameterCaseInsensitive(prices, stakeTokenAddress)?.usd;
+    const rewardTokenPrice = getParameterCaseInsensitive(prices, rewardTokenAddress)?.usd;
+
+    const staked_tvl = poolPrices.staked_tvl;
+
+    const userStaked = rewardTokens.usersBalance / 10 ** stakeToken.decimals;
+
+    const userUnstaked = stakeToken.unstaked;
+
+    return {
+      stakingAddress,
+      poolPrices,
+      stakeTokenAddress,
+      rewardTokens,
+      stakeTokenTicker,
+      stakeTokenPrice,
+      staked_tvl,
+      userStaked,
+      userUnstaked
+    }
 }
