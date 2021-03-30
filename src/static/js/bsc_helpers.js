0cdc06116f07e352bfbec5f7f7537ec4ecf538a2
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-03-30 12:58:29 +0300

    Added popsicle bsc and eth and muscle

--- src/static/js/bsc_helpers.js
+++ src/static/js/bsc_helpers.js
@@ -517,7 +517,8 @@ const bscTokens = [
   { "id": "tsuki-dao", "symbol": "TSUKI", "contract": "0x3fd9e7041c45622e8026199a46f763c9807f66f3"},
   { "id": "panda-yield", "symbol": "BBOO", "contract": "0xd909840613fcb0fadc6ee7e5ecf30cdef4281a68"},
   { "id": "cryptex", "symbol": "CRX", "contract": "0x97a30C692eCe9C317235d48287d23d358170FC40"},
-  { "id": "polis", "symbol": "POLIS", "contract": "0xb5bea8a26d587cf665f2d78f077cca3c7f6341bd"}
+  { "id": "polis", "symbol": "POLIS", "contract": "0xb5bea8a26d587cf665f2d78f077cca3c7f6341bd"},
+  { "id": "tether", "symbol": "USDT", "contract": "0x049d68029688eAbF473097a2fC38ef61633A3C7A"}
 ]
 
 async function getBscPrices() {
