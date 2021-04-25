2933173b7452a2b59eca3b7ccdfb1269af629022
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-04-25 16:51:58 +0300

    changes at galaxy

--- src/static/js/matic_galaxy.js
+++ src/static/js/matic_galaxy.js
@@ -16,14 +16,14 @@ async function main() {
   const prices = await getMaticPrices();
 
   const pools = GALAXY_POOLS.map(c => { return {
-    address: c.stakingRewardAddress,
+    address: c,
     abi: GALAXY_STAKING_ABI,
     stakeTokenFunction: "stakingToken",
     rewardTokenFunction: "rewardsToken"
   }})
 
-  await loadMaticSynthetixPoolInfo(App, tokens, prices, pools[1].abi, pools[1].address,
-    pools[1].rewardTokenFunction, pools[1].stakeTokenFunction)
+  //await loadMaticSynthetixPoolInfo(App, tokens, prices, pools[1].abi, pools[1].address,
+    //pools[1].rewardTokenFunction, pools[1].stakeTokenFunction)
 
   let p = await loadMultipleMaticSynthetixPools(App, tokens, prices, pools)
   _print_bold(`Total staked: $${formatMoney(p.staked_tvl)}`);
@@ -35,6 +35,20 @@ async function main() {
 }
 
 const GALAXY_POOLS = [
+  "0x1ef43d468D8Be2771646D7582e0d8A96DC64CAe8",
+  "0xba847Ca5a3D361F0902FbE13526B68e78E70Daf5",
+  "0x29028198437136DDfcF00D0a0365202a51BB00A4",
+  "0xfE7FfA5E1d645F8e95b9A7D2d5895515A18ebC5F",
+  "0xd6098C3Fb1b425D1016C779bAd675abeE74576B8",
+  "0x2F859D249C2BB59c0278E21CcbaC1C46a32A380f",
+  "0xaeE8E3edeFf09eB6b24051114974cEcb8b8422e4",
+  "0x1C8cC4822def4f0f14Bd3C8A26a150c1B0BFd89D",
+  "0x7E53eCAaeeCb5d4c0A1F33884aeb6E1Bd25E6fdf",
+  "0x3Cc167eFA281f1a6120f96aF2F2537d6DdD469ac",
+  "0xB3a2bA62802dF29e32Bb78Ff58f0dED8a36c321d"
+]
+
+/*const GALAXY_POOLS = [
   "0x20f0ea9fe25efd3ae5cd73b0f010c57dd38a2e46",
   "0x1ef5f6248bd4f7b54fdc9ff35ce9479b702add9b",
   "0x1b25321cb3cdfbfd86cae2249a38365a498dc036",
@@ -51,4 +65,4 @@ const GALAXY_POOLS = [
   "0x7E53eCAaeeCb5d4c0A1F33884aeb6E1Bd25E6fdf",
   "0x3Cc167eFA281f1a6120f96aF2F2537d6DdD469ac",
   "0xB3a2bA62802dF29e32Bb78Ff58f0dED8a36c321d"
-]
+]*/
