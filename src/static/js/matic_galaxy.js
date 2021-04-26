f33e0bc82ae91ca09e91dbe14cda3405a9c1ea0c
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-04-26 13:05:12 +0300

    changes at galaxy

--- src/static/js/matic_galaxy.js
+++ src/static/js/matic_galaxy.js
@@ -4,6 +4,20 @@ $(function() {
   start(main);
 });
 
+const GALAXY_POOLS = [
+  "0x1ef43d468D8Be2771646D7582e0d8A96DC64CAe8",
+  "0xba847Ca5a3D361F0902FbE13526B68e78E70Daf5",
+  "0x29028198437136DDfcF00D0a0365202a51BB00A4",
+  "0xfE7FfA5E1d645F8e95b9A7D2d5895515A18ebC5F",
+  "0xd6098C3Fb1b425D1016C779bAd675abeE74576B8",
+  "0x2F859D249C2BB59c0278E21CcbaC1C46a32A380f",
+  "0xaeE8E3edeFf09eB6b24051114974cEcb8b8422e4",
+  "0x1C8cC4822def4f0f14Bd3C8A26a150c1B0BFd89D",
+  "0x7E53eCAaeeCb5d4c0A1F33884aeb6E1Bd25E6fdf",
+  "0x3Cc167eFA281f1a6120f96aF2F2537d6DdD469ac",
+  "0xB3a2bA62802dF29e32Bb78Ff58f0dED8a36c321d"
+]
+
 const GALAXY_STAKING_ABI = [{"type":"constructor","stateMutability":"nonpayable","payable":false,"inputs":[{"type":"address","name":"_rewardsDistribution","internalType":"address"},{"type":"address[]","name":"_rewardTokens","internalType":"address[]"},{"type":"address","name":"_stakingToken","internalType":"address"}]},{"type":"event","name":"RewardAdded","inputs":[{"type":"uint256","name":"reward","internalType":"uint256","indexed":false}],"anonymous":false},{"type":"event","name":"RewardPaid","inputs":[{"type":"address","name":"user","internalType":"address","indexed":true},{"type":"uint256","name":"reward","internalType":"uint256","indexed":false}],"anonymous":false},{"type":"event","name":"Staked","inputs":[{"type":"address","name":"user","internalType":"address","indexed":true},{"type":"uint256","name":"amount","internalType":"uint256","indexed":false}],"anonymous":false},{"type":"event","name":"Withdrawn","inputs":[{"type":"address","name":"user","internalType":"address","indexed":true},{"type":"uint256","name":"amount","internalType":"uint256","indexed":false}],"anonymous":false},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"balanceOf","inputs":[{"type":"address","name":"account","internalType":"address"}],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"earned","inputs":[{"type":"address","name":"account","internalType":"address"},{"type":"address","name":"rewardToken","internalType":"address"}],"constant":true},{"type":"function","stateMutability":"nonpayable","payable":false,"outputs":[],"name":"exit","inputs":[],"constant":false},{"type":"function","stateMutability":"nonpayable","payable":false,"outputs":[],"name":"getReward","inputs":[],"constant":false},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"getRewardForDuration","inputs":[{"type":"address","name":"rewardToken","internalType":"address"}],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"lastTimeRewardApplicable","inputs":[],"constant":true},{"type":"function","stateMutability":"nonpayable","payable":false,"outputs":[],"name":"notifyRewardAmount","inputs":[{"type":"address","name":"rewardToken","internalType":"address"},{"type":"uint256","name":"reward","internalType":"uint256"}],"constant":false},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"periodFinish","inputs":[],"constant":true},{"type":"function","stateMutability":"nonpayable","payable":false,"outputs":[],"name":"rescueFunds","inputs":[{"type":"address","name":"tokenAddress","internalType":"address"},{"type":"address","name":"receiver","internalType":"address"}],"constant":false},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"rewardLastUpdatedTime","inputs":[{"type":"address","name":"","internalType":"address"}],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"rewardPerToken","inputs":[{"type":"address","name":"rewardToken","internalType":"address"}],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"rewardPerTokenStored","inputs":[],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"address","name":"","internalType":"address"}],"name":"rewardTokens","inputs":[{"type":"uint256","name":"","internalType":"uint256"}],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"rewards","inputs":[{"type":"address","name":"","internalType":"address"},{"type":"address","name":"","internalType":"address"}],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"address","name":"","internalType":"address"}],"name":"rewardsDistribution","inputs":[],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"rewardsDuration","inputs":[],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"rewardsPerTokenMap","inputs":[{"type":"address","name":"","internalType":"address"}],"constant":true},{"type":"function","stateMutability":"nonpayable","payable":false,"outputs":[],"name":"stake","inputs":[{"type":"uint256","name":"amount","internalType":"uint256"}],"constant":false},{"type":"function","stateMutability":"nonpayable","payable":false,"outputs":[],"name":"stakeWithPermit","inputs":[{"type":"uint256","name":"amount","internalType":"uint256"},{"type":"uint256","name":"deadline","internalType":"uint256"},{"type":"uint8","name":"v","internalType":"uint8"},{"type":"bytes32","name":"r","internalType":"bytes32"},{"type":"bytes32","name":"s","internalType":"bytes32"}],"constant":false},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"address[]","name":"","internalType":"address[]"},{"type":"uint256[]","name":"","internalType":"uint256[]"}],"name":"stakerBalances","inputs":[],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"address","name":"","internalType":"contract IERC20"}],"name":"stakingToken","inputs":[],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"tokenRewardRate","inputs":[{"type":"address","name":"","internalType":"address"}],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"totalSupply","inputs":[],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"userRewardPerTokenPaid","inputs":[{"type":"address","name":"","internalType":"address"},{"type":"address","name":"","internalType":"address"}],"constant":true},{"type":"function","stateMutability":"nonpayable","payable":false,"outputs":[],"name":"withdraw","inputs":[{"type":"uint256","name":"amount","internalType":"uint256"}],"constant":false}]
 
 async function main() {  
@@ -22,12 +36,7 @@ async function main() {
     rewardTokenFunction: "rewardTokens"
   }})
 
-  //There are more than one rewards, rewardTokens function needs a parameter
-
-  //await loadMaticSynthetixPoolInfo(App, tokens, prices, pools[1].abi, pools[1].address,
-    //pools[1].rewardTokenFunction, pools[1].stakeTokenFunction)
-
-  let p = await loadMultipleMaticSynthetixPools(App, tokens, prices, pools)
+  let p = await loadGalaxySynthetixPools(App, tokens, prices, pools)
   _print_bold(`Total staked: $${formatMoney(p.staked_tvl)}`);
   if (p.totalUserStaked > 0) {
     _print(`You are staking a total of $${formatMoney(p.totalUserStaked)} at an APR of ${(p.totalAPR * 100).toFixed(2)}%\n`);
@@ -36,35 +45,178 @@ async function main() {
   hideLoading();
 }
 
-const GALAXY_POOLS = [
-  "0x1ef43d468D8Be2771646D7582e0d8A96DC64CAe8",
-  "0xba847Ca5a3D361F0902FbE13526B68e78E70Daf5",
-  "0x29028198437136DDfcF00D0a0365202a51BB00A4",
-  "0xfE7FfA5E1d645F8e95b9A7D2d5895515A18ebC5F",
-  "0xd6098C3Fb1b425D1016C779bAd675abeE74576B8",
-  "0x2F859D249C2BB59c0278E21CcbaC1C46a32A380f",
-  "0xaeE8E3edeFf09eB6b24051114974cEcb8b8422e4",
-  "0x1C8cC4822def4f0f14Bd3C8A26a150c1B0BFd89D",
-  "0x7E53eCAaeeCb5d4c0A1F33884aeb6E1Bd25E6fdf",
-  "0x3Cc167eFA281f1a6120f96aF2F2537d6DdD469ac",
-  "0xB3a2bA62802dF29e32Bb78Ff58f0dED8a36c321d"
-]
+async function loadGalaxySynthetixPools(App, tokens, prices, pools) {
+  let totalStaked  = 0, totalUserStaked = 0, individualAPRs = [];
+  const infos = await Promise.all(pools.map(p => 
+      loadGalaxyPoolInfo(App, tokens, prices, p.abi, p.address, p.rewardTokenFunction, p.stakeTokenFunction)));
+  for (const i of infos.filter(i => i?.poolPrices)) {
+    let p = await printGalaxyPool(App, i, "matic");
+    totalStaked += p.staked_tvl || 0;
+    totalUserStaked += p.userStaked || 0;
+    if (p.userStaked > 0) {
+      individualAPRs.push(p.userStaked * p.apr / 100);
+    }
+  }
+  let totalAPR = totalUserStaked == 0 ? 0 : individualAPRs.reduce((x,y)=>x+y, 0) / totalUserStaked;
+  return { staked_tvl : totalStaked, totalUserStaked, totalAPR };
+}
 
-/*const GALAXY_POOLS = [
-  "0x20f0ea9fe25efd3ae5cd73b0f010c57dd38a2e46",
-  "0x1ef5f6248bd4f7b54fdc9ff35ce9479b702add9b",
-  "0x1b25321cb3cdfbfd86cae2249a38365a498dc036",
-  "0x47192bbfded719ecdda66790d841c3cac91ada46",
-  "0xe0bd98181c73cdb22ccaeb8016d8a92700c8843d",
-  "0x1ef43d468D8Be2771646D7582e0d8A96DC64CAe8",
-  "0xba847Ca5a3D361F0902FbE13526B68e78E70Daf5",
-  "0x29028198437136DDfcF00D0a0365202a51BB00A4",
-  "0xfE7FfA5E1d645F8e95b9A7D2d5895515A18ebC5F",
-  "0xd6098C3Fb1b425D1016C779bAd675abeE74576B8",
-  "0x2F859D249C2BB59c0278E21CcbaC1C46a32A380f",
-  "0xaeE8E3edeFf09eB6b24051114974cEcb8b8422e4",
-  "0x1C8cC4822def4f0f14Bd3C8A26a150c1B0BFd89D",
-  "0x7E53eCAaeeCb5d4c0A1F33884aeb6E1Bd25E6fdf",
-  "0x3Cc167eFA281f1a6120f96aF2F2537d6DdD469ac",
-  "0xB3a2bA62802dF29e32Bb78Ff58f0dED8a36c321d"
-]*/
+async function loadGalaxyPoolInfo(App, tokens, prices, stakingAbi, stakingAddress,
+  rewardTokenFunction, stakeTokenFunction) {
+    const STAKING_POOL = new ethers.Contract(stakingAddress, stakingAbi, App.provider);
+
+    if (!STAKING_POOL.callStatic[stakeTokenFunction]) {
+      console.log("Couldn't find stake function ", stakeTokenFunction);
+    }
+    const stakeTokenAddress = await STAKING_POOL.callStatic[stakeTokenFunction]();
+
+    let rewardTokenAddresses = [];
+    for(let i = 0; i < 11; i++){
+      let rewardTokenAddress = await STAKING_POOL.callStatic[rewardTokenFunction](i);
+      rewardTokenAddresses.push(rewardTokenAddress);
+    }
+
+    var stakeToken = await getMaticToken(App, stakeTokenAddress, stakingAddress);
+
+    var newTokenAddresses = stakeToken.tokens.filter(x =>
+      !getParameterCaseInsensitive(tokens,x));
+    for (const address of newTokenAddresses) {
+        tokens[address] = await getMaticToken(App, address, stakingAddress);
+    }
+    let rewardTokenTickers = [];
+    let earneds = [];
+    for(const rewardTokenAddress of rewardTokenAddresses){
+      tokens[rewardTokenAddress] = await getMaticToken(App, rewardTokenAddress, stakingAddress);
+      let rewardToken = getParameterCaseInsensitive(tokens, rewardTokenAddress);
+      let earned = await STAKING_POOL.earned(App.YOUR_ADDRESS, rewardTokenAddress) / 10 ** rewardToken.decimals;
+      let rewardTokenTicker = rewardToken.symbol;
+      rewardTokenTickers.push(rewardTokenTicker);
+      earneds.push(earned);
+    }
+
+    const poolPrices = getPoolPrices(tokens, prices, stakeToken, "matic");
+
+    if (!poolPrices) 
+    {
+      console.log(`Couldn't calculate prices for pool ${stakeTokenAddress}`);
+      return null;
+    }
+
+    const stakeTokenTicker = poolPrices.stakeTokenTicker;
+
+    const stakeTokenPrice =
+        prices[stakeTokenAddress]?.usd ?? getParameterCaseInsensitive(prices, stakeTokenAddress)?.usd;
+
+    const periodFinish = await STAKING_POOL.periodFinish();
+    const rewardRate = await STAKING_POOL.rewardRate();
+
+    let weeklyRewards = [];
+    let rewardTokenPrices = [];
+    let usdCoinsPerWeek = [];
+
+    for(const rewardTokenAddress of rewardTokenAddresses){
+      let rewardTokenPrice = getParameterCaseInsensitive(prices, rewardTokenAddress)?.usd;
+      rewardTokenPrices.push(rewardTokenPrice);
+      let weeklyReward = (Date.now() / 1000 > periodFinish) ? 0 : rewardRate / 1e18 * 604800;
+      weeklyRewards.push(weeklyReward);
+      let usdPerWeek = weeklyReward * rewardTokenPrice;
+      usdCoinsPerWeek.push(usdPerWeek);
+    }
+
+    const staked_tvl = poolPrices.staked_tvl;
+
+    const userStaked = await STAKING_POOL.balanceOf(App.YOUR_ADDRESS) / 10 ** stakeToken.decimals;
+
+    const userUnstaked = stakeToken.unstaked;
+
+    return  {
+      stakingAddress,
+      poolPrices,
+      stakeTokenAddress,
+      rewardTokenAddress,
+      stakeTokenTicker,
+      rewardTokenTicker,
+      stakeTokenPrice,
+      rewardTokenPrices,
+      weeklyRewards,
+      usdCoinsPerWeek,
+      staked_tvl,
+      userStaked,
+      userUnstaked,
+      earneds
+    }
+}
+
+async function printGalaxyPool(App, info, chain="eth") {
+  info.poolPrices.print_price(chain);
+  _print(`${info.rewardTokenTicker} Per Week: ${info.weeklyRewards.toFixed(2)} ($${formatMoney(info.usdPerWeek)})`);
+  const weeklyAPR = info.usdPerWeek / info.staked_tvl * 100;
+  const dailyAPR = weeklyAPR / 7;
+  const yearlyAPR = weeklyAPR * 52;
+  _print(`APR: Day ${dailyAPR.toFixed(2)}% Week ${weeklyAPR.toFixed(2)}% Year ${yearlyAPR.toFixed(2)}%`);
+  const userStakedUsd = info.userStaked * info.stakeTokenPrice;
+  const userStakedPct = userStakedUsd / info.staked_tvl * 100;
+  _print(`You are staking ${info.userStaked.toFixed(6)} ${info.stakeTokenTicker} ` +
+         `$${formatMoney(userStakedUsd)} (${userStakedPct.toFixed(2)}% of the pool).`);
+  if (info.userStaked > 0) {
+    info.poolPrices.print_contained_price(info.userStaked);
+      const userWeeklyRewards = userStakedPct * info.weeklyRewards / 100;
+      const userDailyRewards = userWeeklyRewards / 7;
+      const userYearlyRewards = userWeeklyRewards * 52;
+      _print(`Estimated ${info.rewardTokenTicker} earnings:`
+          + ` Day ${userDailyRewards.toFixed(2)} ($${formatMoney(userDailyRewards*info.rewardTokenPrice)})`
+          + ` Week ${userWeeklyRewards.toFixed(2)} ($${formatMoney(userWeeklyRewards*info.rewardTokenPrice)})`
+          + ` Year ${userYearlyRewards.toFixed(2)} ($${formatMoney(userYearlyRewards*info.rewardTokenPrice)})`);
+  }
+  const approveTENDAndStake = async function() {
+    return rewardsContract_stake(info.stakeTokenAddress, info.stakingAddress, App)
+  }
+  const unstake = async function() {
+    return rewardsContract_unstake(info.stakingAddress, App)
+  }
+  const claim = async function() {
+    return rewardsContract_claim(info.stakingAddress, App)
+  }
+  const exit = async function() {
+    return rewardsContract_exit(info.stakingAddress, App)
+  }
+  const revoke = async function() {
+    return rewardsContract_resetApprove(info.stakeTokenAddress, info.stakingAddress, App)
+  }
+  switch (chain) {
+    case "eth":
+      _print(`<a target="_blank" href="https://etherscan.io/address/${info.stakingAddress}#code">Etherscan</a>`);
+      break;
+    case "avax":
+      _print(`<a target="_blank" href="https://cchain.explorer.avax.network/address/${info.stakingAddress}#code">Explorer</a>`);
+      break;
+    case "bsc":
+      _print(`<a target="_blank" href="https://bscscan.com/address/${info.stakingAddress}#code">BSC Scan</a>`);
+      break;
+    case "heco":
+      _print(`<a target="_blank" href="https://hecoinfo.com/address/${info.stakingAddress}#code">Heco Scan</a>`);
+      break;
+    case "matic":
+      _print(`<a target="_blank" href="https://explorer-mainnet.maticvigil.com/address/${info.stakingAddress}#code">Matic Explorer</a>`);
+      break;
+  }
+  if (info.stakeTokenTicker != "ETH") {
+    _print_link(`Stake ${info.userUnstaked.toFixed(6)} ${info.stakeTokenTicker}`, approveTENDAndStake)
+  }
+  else {
+    _print("Please use the official website to stake ETH.");
+  }
+  _print_link(`Unstake ${info.userStaked.toFixed(6)} ${info.stakeTokenTicker}`, unstake)
+  _print_link(`Claim ${info.earned.toFixed(6)} ${info.rewardTokenTicker} ($${formatMoney(info.earned*info.rewardTokenPrice)})`, claim)
+  if (info.stakeTokenTicker != "ETH") {
+    _print_link(`Revoke (set approval to 0)`, revoke)
+  }
+  _print_link(`Exit`, exit)
+  _print("");
+
+  return {
+      staked_tvl: info.poolPrices.staked_tvl,
+      userStaked : userStakedUsd,
+      apr : yearlyAPR
+  }
+}
