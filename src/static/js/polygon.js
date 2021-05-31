56b19015c7f0cb3cb926eb5a83e38393c17309b7
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-05-31 11:35:46 +0300

    Added polyswan and polyion

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -60,7 +60,9 @@ const main = async() => {
         ["Polydex            ",`<a href="polydex"         >Various</a>`,"PLX          ","https://polydex.fi"],
         ["PolyMaple          ",`<a href="polymaple"       >Various</a>`,"MAPLE        ",""],
         ["Gravity Finance    ",`<a href="gravityfinance"  >Various</a>`,"GFI          ","https://gravityfinance.io"],
-        ["Smellycat          ",`<a href="smellycat"       >Various</a>`,"PUSSY        ","https://www.smellycat.finance"]
+        ["Smellycat          ",`<a href="smellycat"       >Various</a>`,"PUSSY        ","https://www.smellycat.finance"],
+        ["Polyswan           ",`<a href="polyswan"        >Various</a>`,"SWAN         ","https://www.polyswan.finance"],
+        ["Polyion            ",`<a href="polyion"         >Various</a>`,"LION         ","https://www.polyion.net"]
 
       ]
   }
