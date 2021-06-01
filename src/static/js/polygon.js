84fd08222241e7318220277dd240c5007f7de5a2
piratejeff <piratejeff@tutanota.com>
2021-06-01 14:34:12 +0100

    feat: pirate dice

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -64,7 +64,8 @@ const main = async() => {
         ["Polygold           ",`<a href="polygold"       >Various</a>`,"POLYGOLD      ","https://polygold.finance"],
         ["Polyswan           ",`<a href="polyswan"        >Various</a>`,"SWAN         ","https://www.polyswan.finance"],
         ["Polyion            ",`<a href="polyion"         >Various</a>`,"LION         ","https://www.polyion.net"],
-        ["Eleven             ",`<a href="eleven"          >Various</a>`,"ELE          ","https://eleven.finance"]
+        ["Eleven             ",`<a href="eleven"          >Various</a>`,"ELE          ","https://eleven.finance"],
+        ["Pirate Dice        ",`<a href="piratedice"      >Various</a>`,"BOOTY        ","http://piratedice.xyz"]
       ]
   }
 
