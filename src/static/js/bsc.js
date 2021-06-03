0350dedfa66712d19958cf5c96fb73fba678e4cf
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-06-03 13:30:12 +0300

    Added fishswap

--- src/static/js/bsc.js
+++ src/static/js/bsc.js
@@ -302,7 +302,8 @@ const main = async() => {
       ["Chained Farms Network (SFN1)", `<a href="chainedfarmsnetwork_sub1">Various</a>`,"SFN1          ","https://sub1.chainedfarms.network"],
       ["Prism               ", `<a href="prism"         >Various</a>`,"PRISM         ","https://bsc.prismfinance.net"],
       ["Aloe                ", `<a href="aloe"          >Various</a>`,"ALOE          ","https://aloeswap.com"],
-      ["Tea                 ", `<a href="tea"           >Various</a>`,"SUGAR         ","https://tea-swap-app.finance"]
+      ["Tea                 ", `<a href="tea"           >Various</a>`,"SUGAR         ","https://tea-swap-app.finance"],
+      ["Fish                ", `<a href="fish"          >Various</a>`,"FISH          ","https://fishswap.apps"]
     ]
   }
 
