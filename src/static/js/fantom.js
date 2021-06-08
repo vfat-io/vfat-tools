95d9384afa68ba43f3841db9211674fb62f2e6ca
AquariusFi <contact@aquarius.fi>
2021-06-08 11:12:39 +0800

    add aquarius

--- src/static/js/fantom.js
+++ src/static/js/fantom.js
@@ -1,131 +1,137 @@
-$(function() {
-  main()
+$(function () {
+    main()
 });
 
-const main = async() => {
+const main = async () => {
 
-  let tableData = {
-    "title":"FANTOM Network",
-    "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
-    "rows": [
-      [
-        "Popsicle",
-        `<a href='popsicle'>Various</a>`,
-        "ICE",
-        "https://popsicle.finance"
-      ],
-      [
-        "Spooky",
-        `<a href='spooky'>Various</a>`,
-        "boo",
-        "https://spookyswap.finance"
-      ],
-      [
-        "Spirit",
-        `<a href='spirit'>Various</a>`,
-        "SPIRIT",
-        "https://app.spiritswap.finance"
-      ],
-      [
-        "Borg",
-        `<a href='borg'>Various</a>`,
-        "SON",
-        "https://ftm.borgswap.exchange"
-      ],
-      [
-        "Hyperjump",
-        `<a href='hyperjump'>Various</a>`,
-        "ORI",
-        "https://hyperjump.fi"
-      ],
-      [
-        "Ester",
-        `<a href='ester'>Various</a>`,
-        "EST",
-        "https://ester.finance"
-      ],
-      [
-        "Froyo",
-        `<a href='froyo'>Various</a>`,
-        "FROYO",
-        "https://frozenyogurt.finance"
-      ],
-      [
-        "Ghost",
-        `<a href='ghost'>Various</a>`,
-        "GHOST",
-        "https://www.theghost.finance"
-      ],
-      [
-        "Beefy",
-        `<a href='beefy'>Various</a>`,
-        "Various",
-        "https://fantom.beefy.finance"
-      ],
-      [
-        "Liquid Driver",
-        `<a href='liquid'>Various</a>`,
-        "LQDR",
-        "https://www.liquiddriver.finance"
-      ],
-      [
-        "Standard",
-        `<a href='standard'>Various</a>`,
-        "STRD",
-        "https://strd.finance"
-      ],
-      [
-        "Mushrooms",
-        `<a href='mushrooms'>Various</a>`,
-        "MM",
-        "https://mushrooms.finance/farms"
-      ],
-      [
-        "Greenhouse",
-        `<a href='greenhouse'>Various</a>`,
-        "SEED",
-        "https://thegreenhouse.finance"
-      ],
-      [
-        "Stake Steak",
-        `<a href='steak'>Various</a>`,
-        "STEAK",
-        "https://stakesteak.com"
-      ],
-      [
-        "Frankenstein",
-        `<a href='frankenstein'>Various</a>`,
-        "FRANK",
-        "https://frankenstein.finance"
-      ],
-      [
-        "Waka",
-        `<a href='waka'>Various</a>`,
-        "WAKA",
-        "https://waka.finance"
-      ],
-      [
-        "Elk *",
-        `<a href='elk'>Various</a>`,
-        "ELK",
-        "https://app.elk.finance"
-      ],
-      [
-        "Paintswap",
-        `<a href='paintswap'>Various</a>`,
-        "BRUSH",
-        "https://paintswap.finance"
-      ],
-      [
-        "Tomb",
-        `<a href='tomb'>Various</a>`,
-        "Various",
-        "https://tomb.finance"
-      ]
-    ]
-  }
+    let tableData = {
+        "title": "FANTOM Network",
+        "heading": ["Pool Provider", "LP", "Reward Tokens", "INFO"],
+        "rows": [
+            [
+                "Popsicle",
+                `<a href='popsicle'>Various</a>`,
+                "ICE",
+                "https://popsicle.finance"
+            ],
+            [
+                "Spooky",
+                `<a href='spooky'>Various</a>`,
+                "boo",
+                "https://spookyswap.finance"
+            ],
+            [
+                "Spirit",
+                `<a href='spirit'>Various</a>`,
+                "SPIRIT",
+                "https://app.spiritswap.finance"
+            ],
+            [
+                "Borg",
+                `<a href='borg'>Various</a>`,
+                "SON",
+                "https://ftm.borgswap.exchange"
+            ],
+            [
+                "Hyperjump",
+                `<a href='hyperjump'>Various</a>`,
+                "ORI",
+                "https://hyperjump.fi"
+            ],
+            [
+                "Ester",
+                `<a href='ester'>Various</a>`,
+                "EST",
+                "https://ester.finance"
+            ],
+            [
+                "Froyo",
+                `<a href='froyo'>Various</a>`,
+                "FROYO",
+                "https://frozenyogurt.finance"
+            ],
+            [
+                "Ghost",
+                `<a href='ghost'>Various</a>`,
+                "GHOST",
+                "https://www.theghost.finance"
+            ],
+            [
+                "Beefy",
+                `<a href='beefy'>Various</a>`,
+                "Various",
+                "https://fantom.beefy.finance"
+            ],
+            [
+                "Liquid Driver",
+                `<a href='liquid'>Various</a>`,
+                "LQDR",
+                "https://www.liquiddriver.finance"
+            ],
+            [
+                "Standard",
+                `<a href='standard'>Various</a>`,
+                "STRD",
+                "https://strd.finance"
+            ],
+            [
+                "Mushrooms",
+                `<a href='mushrooms'>Various</a>`,
+                "MM",
+                "https://mushrooms.finance/farms"
+            ],
+            [
+                "Greenhouse",
+                `<a href='greenhouse'>Various</a>`,
+                "SEED",
+                "https://thegreenhouse.finance"
+            ],
+            [
+                "Stake Steak",
+                `<a href='steak'>Various</a>`,
+                "STEAK",
+                "https://stakesteak.com"
+            ],
+            [
+                "Frankenstein",
+                `<a href='frankenstein'>Various</a>`,
+                "FRANK",
+                "https://frankenstein.finance"
+            ],
+            [
+                "Waka",
+                `<a href='waka'>Various</a>`,
+                "WAKA",
+                "https://waka.finance"
+            ],
+            [
+                "Elk *",
+                `<a href='elk'>Various</a>`,
+                "ELK",
+                "https://app.elk.finance"
+            ],
+            [
+                "Paintswap",
+                `<a href='paintswap'>Various</a>`,
+                "BRUSH",
+                "https://paintswap.finance"
+            ],
+            [
+                "Tomb",
+                `<a href='tomb'>Various</a>`,
+                "Various",
+                "https://tomb.finance"
+            ],
+            [
+                "Aquarius",
+                `<a href='aquarius'>Various</a>`,
+                "AQU",
+                "https://aquarius.fi"
+            ]
+        ]
+    }
 
-  let table = new AsciiTable().fromJSON(tableData);
-  document.getElementById('log').innerHTML += table + '<br />';
-  hideLoading();
+    let table = new AsciiTable().fromJSON(tableData);
+    document.getElementById('log').innerHTML += table + '<br />';
+    hideLoading();
 }
