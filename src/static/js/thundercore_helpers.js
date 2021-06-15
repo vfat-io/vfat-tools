8f860a53730a6aeeea8001bb9dbf571ea5d1c67c
Flash Boys <laflashboys@gmail.com>
2021-06-15 12:03:06 +0800

    fix pricing undefined bug

--- src/static/js/thundercore_helpers.js
+++ src/static/js/thundercore_helpers.js
@@ -2,7 +2,8 @@ const ttTokens = [
   { "id": "thunder-token","symbol": "TT","contract": "0x0000000000000000000000000000000000000000" },
   { "id": "usd-coin","symbol": "USDC","contract": "0x22e89898A04eaf43379BeB70bf4E38b1faf8A31e" },
   { "id": "tether", "symbol": "USDT", "contract": "0x4f3C8E20942461e2c3Bdd8311AC57B0c222f2b82"},
-  { "id":"binance-usd", "symbol": "BUSD", "contract": "0xBEB0131D95AC3F03fd15894D0aDE5DBf7451d171" }
+  { "id":"binance-usd", "symbol": "BUSD", "contract": "0xBEB0131D95AC3F03fd15894D0aDE5DBf7451d171" },
+  { "id":"ethereum", "symbol": "ETHER", "contract": "0xBEB0131D95AC3F03fd15894D0aDE5DBf7451d171" }
 ]
 
 async function getTTPrices() {
@@ -41,4 +42,3 @@ async function geterc20(App, token, address, stakingAddress) {
       tokens : [address]
   };
 }
-
