3967d2b49b45096599455c9c32ef47e177a9a06f
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-06-16 07:12:07 +0300

    Added cosmic

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -101,7 +101,8 @@ const main = async() => {
         ["Wault               ",`<a href="wault"           >Various</a>`,"WEXpoly      ","https://app.wault.finance/polygon"],
         ["PolyHarvest         ",`<a href="polyharvest"     >Various</a>`,"WHEAT        ","https://www.polyharvest.finance"],
         ["PolyWoof            ",`<a href="polywoof"        >Various</a>`,"TREAT        ","https://www.polywoof.finance"],
-        ["SafeDollar          ",`<a href="safedollar"      >Various</a>`,"SDS/SDO      ","https://app.safedollar.fi"]
+        ["SafeDollar          ",`<a href="safedollar"      >Various</a>`,"SDS/SDO      ","https://app.safedollar.fi"],
+        ["Cosmic              ",`<a href="cosmic"          >Various</a>`,"COSMIC       ","https://polygon.cosmicswap.finance"]
       ]
   }
 
