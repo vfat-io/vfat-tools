25bb41081d008a071df28774720b62d4dbfa4116
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-06-25 15:36:34 +0300

    Added cookiebake

--- src/static/js/bsc.js
+++ src/static/js/bsc.js
@@ -364,7 +364,8 @@ const main = async() => {
       ["Frozencake          ", `<a href="frozencake"    >Various</a>`,"FROZEN           ","https://www.frozencake.finance"],
       ["Fox Finance         ", `<a href="foxfinance"    >Various</a>`,"FOX              ","https://www.foxfinance.org"],
       ["Cardboard Finance   ", `<a href="cardboard"     >Various</a>`,"CARDBOARD        ","https://cardboard.finance"],
-      ["Yogi                ", `<a href="yogi"          >Various</a>`,"YOGI             ","https://bsc.farms.yogi.fi"]
+      ["Yogi                ", `<a href="yogi"          >Various</a>`,"YOGI             ","https://bsc.farms.yogi.fi"],
+      ["Cookiebake          ", `<a href="cookiebake"    >Various</a>`,"CHIPS            ","https://cookiebake.finance"]
     ]}
 
 
