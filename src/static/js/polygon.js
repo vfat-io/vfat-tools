3ab8c720e2ab9b69cd1f01b9fc5b663912263aa8
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-06-28 13:05:59 +0300

    Added tako

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -348,7 +348,8 @@ const main = async () => {
       ['Rediant             ', `<a href="rediant"         >Various</a>`, 'RDT          ', 'https://rediant.finance'],
       ['StonkZFarm          ', `<a href="stonkzfarm"      >Various</a>`, 'STONKZ       ', 'https://stonkz.farm'],
       ['NinjaSwap           ', `<a href="ninjaswap"       >Various</a>`, 'NINJA        ', 'https://ninjaswapdefi.com'],
-      ['Polyquity           ', `<a href="polyquity"       >Various</a>`, 'PYQ          ', 'https://app.polyquity.org']
+      ['Polyquity           ', `<a href="polyquity"       >Various</a>`, 'PYQ          ', 'https://app.polyquity.org'],
+      ['Tako                ', `<a href="tako"            >Various</a>`, 'TAKO         ', 'https://takodefi.com']
     ],
   }
 
