1fec83141455e8a09fa332220881fa47a2f2a92a
Dimosthenis Tsatsos <36194185+Dimosgr@users.noreply.github.com>
2021-06-29 10:14:58 +0300

    Update polygon.js

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -354,8 +354,8 @@ const main = async () => {
       ['Polybrew            ', `<a href="polybrew"        >Various</a>`, 'BREW         ', 'https://polybrew.finance'],
       ['PolyDiamond         ', `<a href="polydiamond"     >Various</a>`, 'DMND         ', 'https://polydiamondfinance.com'],
       ['PolyCactus          ', `<a href="polycactus"      >Various</a>`, 'CACTUS       ', 'https://polycactus.com'],
-      ['PolyGrass           ', `<a href="polygrass"       >Various</a>`, 'GRASS        ', 'https://polygrass.online']
-      ['Pickle              ', `<a href="pickle"          >Various</a>`, 'PICKLE       ', 'https://app.pickle.finance'],
+      ['PolyGrass           ', `<a href="polygrass"       >Various</a>`, 'GRASS        ', 'https://polygrass.online'],
+      ['Pickle              ', `<a href="pickle"          >Various</a>`, 'PICKLE       ', 'https://app.pickle.finance']
     ],
   }
 
