f90d5f245bafc6ef244c81d057749160c6838be7
Anonymous <anon@anon.com>
2021-07-02 10:21:04 +0200

    Added extra links

--- src/static/js/matic_polytreasure.js
+++ src/static/js/matic_polytreasure.js
@@ -39,7 +39,7 @@ async function main() {
    _print(`|  |  |_____|  |  |  |  |  |  |_____   |  |  |  |  |  |  ${Number(btc).toFixed(3)}    |`);
    _print("|  |_____   |  |__|  |  |  |________|  |  |  |  |__|  |  BTC      |");
    _print("|___________|_______________________|________|________|___________|");
-   _print_link("Claim ETH (needs 1 TRZ tokens!)", async() => {
+   _print_link("Claim ETH (needs 1 TRZ token!)", async() => {
 	   const signer = App.provider.getSigner();
 	   const treasuryContract = new ethers.Contract(TREASURY_ADDR, TREASURY_ABI, signer);
        treasuryContract.claimETH(ethers.utils.parseUnits("1", 18), {gasLimit: 500000});
