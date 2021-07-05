332c424c5a5da38e5d0f4075ce6c547622523707
bisonfarm <86850967+bisonfarm@users.noreply.github.com>
2021-07-05 21:53:42 +0530

    bisonfinance

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -392,7 +392,7 @@ const main = async () => {
       ['Polyyearn           ', `<a href="polyyearn"       >Various</a>`, 'YEARN        ', 'https://polyyearn.finance'],
       ['WorldSwap Hanoi     ', `<a href="worldswap_han"   >Various</a>`, 'HAN          ', 'https://hanoi.worldswap.finance'],
       ['Astrobunny          ', `<a href="astrobunny"      >Various</a>`, 'ABUNNY       ', 'https://www.astrobunny.finance'],
-      ['BisonFarms          ', `<a href="bisonfarms"      >Various</a>`, 'BISON        ', 'https://bisonfinance.online'],
+      ['BisonFinance        ', `<a href="bisonfinance"    >Various</a>`, 'BISON        ', 'https://bisonfinance.online'],
       ['Bullrun             ', `<a href="bullrun"         >Various</a>`, 'BULL         ', 'https://polygon.bullrun.finance'],
       ['PolyArrow           ', `<a href="polyarrow"       >Various</a>`, 'ARROW        ', 'https://polyarrow.xyz'],
       ['PantsuSwap          ', `<a href="pantsuswap"      >Various</a>`, 'PANTSU       ', 'https://pantsuswap.com'],
