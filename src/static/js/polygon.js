3b0b417d5dead799a8436c3943f82c3ee6ed6283
gabbywfx <wkkg11@gmail.com>
2021-07-23 05:58:00 +0800

    Update polygon.js

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -462,6 +462,7 @@ const main = async () => {
       ['PolyBakerySwap      ', `<a href="polybakeryswap"  >Various</a>`, 'BAKE         ', 'https://www.bakery-swap.finance'],
 	  ['PolyTail                ', `<a href="polytail"            >Various</a>`, 'POLYTAIL         ', 'https://polytail.farm'],
       ['PearZap             ', `<a href="pearzap"         >Various</a>`, 'PEAR         ', 'https://pearzap.com']
+      ['Cygnus Finance      ', `<a href="cygnusfinance"  >Various</a>`, 'CYGNUS         ', 'https://www.cygnusdefi.com'],
     ],
   }
 
