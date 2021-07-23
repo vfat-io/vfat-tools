1a2dc3ea773dbc750e773aaa64306f97da81af2d
Ben Kanbour <astrologyfinance@protonmail.com>
2021-07-22 21:44:57 -0600

    added Astrofarms - Neptune

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -460,8 +460,9 @@ const main = async () => {
       ['BlackSwan           ', `<a href="blackswan"       >Various</a>`, 'SWAN         ', 'https://app.blackswan.network'],
       ['PolyBass            ', `<a href="polybass"        >Various</a>`, 'BASS         ', 'https://polybass.farm'],
       ['PolyBakerySwap      ', `<a href="polybakeryswap"  >Various</a>`, 'BAKE         ', 'https://www.bakery-swap.finance'],
-	  ['PolyTail                ', `<a href="polytail"            >Various</a>`, 'POLYTAIL         ', 'https://polytail.farm'],
+	    ['PolyTail            ', `<a href="polytail"        >Various</a>`, 'POLYTAIL     ', 'https://polytail.farm'],
       ['PearZap             ', `<a href="pearzap"         >Various</a>`, 'PEAR         ', 'https://pearzap.com']
+      ['Astrofarms - Neptune', `<a href="astrofarmsneptune">Various</a>`,'NEPTUNE      ', 'https://neptune.astrofarms.finance']
     ],
   }
 
