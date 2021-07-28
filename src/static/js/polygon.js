50d3c5975ef41096835affb8380a889134180f06
miniCow <polycow-finance@pm.me>
2021-07-28 13:46:59 +0200

    BUTTER LAYER

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -418,7 +418,7 @@ const main = async () => {
       ['Polymax - Berkshire ', `<a href="polymax_berkshire">Various</a>`, 'BERKSHIRE   ', 'https://berkshire.polymax.club'],
       ['Platinum Finance    ', `<a href="platinumfinance" >Various</a>`, 'PLATIN       ', 'https://platinumfinance.app'],
       ['DuckFarm            ', `<a href="duckfarm"        >Various</a>`, 'DUCK         ', 'https://duckfarm.one'],
-      ['PolyCow             ', `<a href="polycow"         >Various</a>`, 'MILK         ', 'https://polycow.finance']
+      ['PolyCow             ', `<a href="polycow"         >Various</a>`, 'MILK / BUTTER', 'https://polycow.finance']
     ],
   }
 
