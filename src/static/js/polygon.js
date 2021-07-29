fe1b214d9c31cf46cb8beb22a64ab73b356686a0
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-07-29 11:16:05 +0300

    Added polyfund

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -479,7 +479,8 @@ const main = async () => {
       ['SushiFarm Finance   ', `<a href="sushifarm"       >Various</a>`, 'SUSHI        ', 'https://sushifarm.finance'],
       ['MeebMaster          ', `<a href="meeb"            >Various</a>`, 'MEEB         ', 'https://app.meebmaster.com'],
       ['Atma                ', `<a href="atma"            >Various</a>`, 'ATMA         ', 'https://0xatma.finance'],
-      ['Avengers Defi       ', `<a href="avengers_defi"   >Various</a>`, 'AVENGERS     ', 'https://avengersdefi.club']
+      ['Avengers Defi       ', `<a href="avengers_defi"   >Various</a>`, 'AVENGERS     ', 'https://avengersdefi.club'],
+      ['PolyFund            ', `<a href="polyfund"        >Various</a>`, 'FUND         ', 'https://www.polyfund.finance']
     ],
   }
 
