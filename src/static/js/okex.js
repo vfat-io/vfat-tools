12b6fc2fd35947f9f4d56956262bd45ad1bb0b29
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-07-30 13:12:20 +0300

    Added kswap

--- src/static/js/okex.js
+++ src/static/js/okex.js
@@ -9,7 +9,8 @@ const main = async() => {
     "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
     "rows": [
       ["Sakeswap",   `<a href='sakeswap'>Various</a>`,  "SAKE",    "https://okapp.sakeswap.fi"],
-      ["WePiggy",    `<a href='wepiggy' >Various</a>`,  "WPC ",    "https://okexchain.wepiggy.com/markets"],
+      ["WePiggy",    `<a href='wepiggy' >Various</a>`,  "WPC",     "https://okexchain.wepiggy.com/markets"],
+      ["KSwap",      `<a href='kswap'   >Various</a>`,  "KST",     "https://kswap.finance"]
     ]
   }
 
