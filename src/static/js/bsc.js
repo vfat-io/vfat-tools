9a211c0baf2ba413def4da499280d61a8bf584a6
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-07-31 10:10:48 +0300

    Added 1001nights

--- src/static/js/bsc.js
+++ src/static/js/bsc.js
@@ -457,7 +457,8 @@ const main = async() => {
       ["Arena               ", `<a href="arena"         >Various</a>`,"ARENA            ","https://www.arenaswap.com"],
       ["Cherry              ", `<a href="cherry"        >Various</a>`,"CHERRY           ","https://www.fletaconnect.io"],
       ["TeaBags             ", `<a href="teabags"       >Various</a>`,"TEABAGS          ","https://www.teabagsdefi.com"],
-      ["HoneyFarm           ", `<a href="honeyfarm"     >Various</a>`,"HONEY            ","https://honeyfarm.finance"]
+      ["HoneyFarm           ", `<a href="honeyfarm"     >Various</a>`,"HONEY            ","https://honeyfarm.finance"],
+      ["1001 Nights         ", `<a href="aladdin"       >Various</a>`,"ALADDIN          ","https://1001nights.gold"]
     ]}
 
   let table = new AsciiTable().fromJSON(tableData);
