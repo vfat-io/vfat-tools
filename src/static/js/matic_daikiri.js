f09dd5d9011d309e9e5b8280d8ada9d8e22230e3
daikirifinance <admin@daikiri.finance>
2021-08-10 13:09:18 -0500

    daikiri matic fix

--- src/static/js/matic_daikiri.js
+++ src/static/js/matic_daikiri.js
@@ -25,7 +25,105 @@ async function main() {
     const prices = await getMaticPrices();
 
     await loadMaticChefContract(App, tokens, prices, DAIKI_CHEF, DAIKI_CHEF_ADDR, DAIKI_CHEF_ABI, rewardTokenTicker,
-        "rewardToken", null, rewardsPerWeek, "pendingRewardToken", [0,1,2,3,4]);
+        "rewardToken", null, rewardsPerWeek, "pendingRewardToken", [0, 1, 2, 3, 4]);
 
     hideLoading();
+}
+
+
+async function loadMaticChefContract(App, tokens, prices, chef, chefAddress, chefAbi, rewardTokenTicker,
+    rewardTokenFunction, rewardsPerBlockFunction, rewardsPerWeekFixed, pendingRewardsFunction,
+    deathPoolIndices) {
+    const chefContract = chef ?? new ethers.Contract(chefAddress, chefAbi, App.provider);
+
+    const poolCount = parseInt(await chefContract.poolLength(), 10);
+    const totalAllocPoints = await chefContract.totalAllocPoint();
+
+    _print(`Found ${poolCount} pools.\n`)
+
+    _print(`Showing incentivized pools only.\n`);
+
+    var tokens = {};
+
+    const rewardTokenAddress = await chefContract.callStatic[rewardTokenFunction]();
+    const rewardToken = await getMaticToken(App, rewardTokenAddress, chefAddress);
+    const rewardsPerWeek = rewardsPerWeekFixed ??
+        await chefContract.callStatic[rewardsPerBlockFunction]()
+        / 10 ** rewardToken.decimals * 604800 / 3
+
+    const poolInfos = await Promise.all([...Array(poolCount).keys()].map(async (x) =>
+        await getMaticPoolInfo(App, chefContract, chefAddress, x, pendingRewardsFunction)));
+
+    var tokenAddresses = [].concat.apply([], poolInfos.filter(x => x.poolToken).map(x => x.poolToken.tokens));
+
+    await Promise.all(tokenAddresses.map(async (address) => {
+        tokens[address] = await getMaticToken(App, address, chefAddress);
+    }));
+
+    if (deathPoolIndices) {   //load prices for the deathpool assets
+        deathPoolIndices.map(i => poolInfos[i])
+            .map(poolInfo =>
+                poolInfo.poolToken ? getPoolPrices(tokens, prices, poolInfo.poolToken, "matic") : undefined);
+    }
+
+    const poolPrices = poolInfos.map(poolInfo => poolInfo.poolToken ? getPoolPrices(tokens, prices, poolInfo.poolToken, "matic") : undefined);
+
+
+    _print("Finished reading smart contracts.\n");
+
+    let aprs = []
+    for (i = 0; i < poolCount; i++) {
+        if (poolPrices[i]) {
+            const apr = printChefPool(App, chefAbi, chefAddress, prices, tokens, poolInfos[i], i, poolPrices[i],
+                totalAllocPoints, rewardsPerWeek, rewardTokenTicker, rewardTokenAddress,
+                pendingRewardsFunction, null, null, "matic", poolInfos[i].depositFee, poolInfos[i].withdrawFee)
+            aprs.push(apr);
+        }
+    }
+    let totalUserStaked = 0, totalStaked = 0, averageApr = 0;
+    for (const a of aprs) {
+        if (!isNaN(a.totalStakedUsd)) {
+            totalStaked += a.totalStakedUsd;
+        }
+        if (a.userStakedUsd > 0) {
+            totalUserStaked += a.userStakedUsd;
+            averageApr += a.userStakedUsd * a.yearlyAPR / 100;
+        }
+    }
+    averageApr = averageApr / totalUserStaked;
+    _print_bold(`Total Staked: $${formatMoney(totalStaked)}`);
+    if (totalUserStaked > 0) {
+        _print_bold(`\nYou are staking a total of $${formatMoney(totalUserStaked)} at an average APR of ${(averageApr * 100).toFixed(2)}%`)
+        _print(`Estimated earnings:`
+            + ` Day $${formatMoney(totalUserStaked * averageApr / 365)}`
+            + ` Week $${formatMoney(totalUserStaked * averageApr / 52)}`
+            + ` Year $${formatMoney(totalUserStaked * averageApr)}\n`);
+    }
+    return { prices, totalUserStaked, totalStaked, averageApr }
+}
+
+async function getMaticPoolInfo(app, chefContract, chefAddress, poolIndex, pendingRewardsFunction) {
+    const poolInfo = await chefContract.poolInfo(poolIndex);
+    if (poolInfo.allocPoint == 0) {
+        return {
+            address: poolInfo.lpToken ?? poolInfo[0],
+            allocPoints: poolInfo.allocPoint ?? 1,
+            poolToken: null,
+            userStaked: 0,
+            pendingRewardTokens: 0,
+        };
+    }
+    const poolToken = await getMaticToken(app, poolInfo.lpToken ?? poolInfo[0], chefAddress);
+    const userInfo = await chefContract.userInfo(poolIndex, app.YOUR_ADDRESS);
+    const pendingRewardTokens = await chefContract.callStatic[pendingRewardsFunction](poolIndex, app.YOUR_ADDRESS);
+    const staked = userInfo.amount / 10 ** poolToken.decimals;
+    return {
+        address: poolInfo.lpToken ?? poolInfo[0],
+        allocPoints: poolInfo.allocPoint ?? 1,
+        poolToken: poolToken,
+        userStaked: staked,
+        pendingRewardTokens: pendingRewardTokens / 10 ** 18,
+        depositFee: (poolInfo.depositFeeBP ?? 0) / 100,
+        withdrawFee: (poolInfo.withdrawFeeBP ?? 0) / 100
+    };
 }
