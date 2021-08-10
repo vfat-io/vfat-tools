8d8f444adaffb355a109e8b4ac03e889a67a8f53
Dimosthenis Tsatsos <36194185+Dimosgr@users.noreply.github.com>
2021-08-10 10:13:18 +0300

    Update matic_helpers.js

--- src/static/js/matic_helpers.js
+++ src/static/js/matic_helpers.js
@@ -397,19 +397,19 @@ async function getMaticPoolInfo(app, chefContract, chefAddress, poolIndex, pendi
   const poolInfo = await chefContract.poolInfo(poolIndex);  
   if (poolInfo.allocPoint == 0) {
     return {
-      address: poolInfo.lpToken ?? poolInfo[0],
+      address: poolInfo.lpToken,
       allocPoints: poolInfo.allocPoint ?? 1,
       poolToken: null,
       userStaked : 0,
       pendingRewardTokens : 0,
     };
   }
-  const poolToken = await getMaticToken(app, poolInfo.lpToken ?? poolInfo[0], chefAddress);
+  const poolToken = await getMaticToken(app, poolInfo.lpToken, chefAddress);
   const userInfo = await chefContract.userInfo(poolIndex, app.YOUR_ADDRESS);
   const pendingRewardTokens = await chefContract.callStatic[pendingRewardsFunction](poolIndex, app.YOUR_ADDRESS);
   const staked = userInfo.amount / 10 ** poolToken.decimals;
   return {
-      address: poolInfo.lpToken ?? poolInfo[0],
+      address: poolInfo.lpToken,
       allocPoints: poolInfo.allocPoint ?? 1,
       poolToken: poolToken,
       userStaked : staked,
