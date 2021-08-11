76a93f6c63eb1554ae17a860ea70726d11672bc6
PolyAnon <88802308+PolyDachs@users.noreply.github.com>
2021-08-11 22:09:49 +0100

    Add PolyDachshund

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -521,6 +521,7 @@ const main = async () => {
       ['Beefarm             ', `<a href="beefarm"         >Various</a>`, 'BEE          ', "https://beefarmpolygon.com"],
       ['Raccoon             ', `<a href="raccoon"         >Various</a>`, 'RACCOON      ', "https://raccoonyield.club"],
       ['PolyFlower          ', `<a href="polyflower"      >Various</a>`, 'FLOWER       ', 'https://t.me/polyflower'],
+      ['PolyDachshund       ', `<a href="polydachs"       >Various</a>`, 'DACHS        ', 'https://t.me/polydachs'],
     ],
   }
 
