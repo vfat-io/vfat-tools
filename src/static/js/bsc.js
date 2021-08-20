d1b12aa45c28917d0efc1b92391c1318c65f1147
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-08-20 17:54:44 +0300

    cookiebake update, added igb, added arable

--- src/static/js/bsc.js
+++ src/static/js/bsc.js
@@ -491,7 +491,8 @@ const main = async() => {
       ["Viralata Finance    ", `<a href="viralata"      >Various</a>`,"VIRALATA         ","https://app.viralata.finance"],
       ["HoneyFarm Bear      ", `<a href="bear"          >Various</a>`,"BEAR             ","https://bear.honeyfarm.finance"],
       ["Treasurefarm        ", `<a href="treasurefarm"  >Various</a>`,"TREASURE         ","https://treasurefarm.cash"],
-      ["BNB STAKE           ", `<a href="bnb_stake"     >STAKE</a>`  ,"BNB              ", "https://t.me/bnb_stake_tg"],
+      ["BNB STAKE           ", `<a href="bnb_stake"     >STAKE</a>`  ,"BNB              ","https://t.me/bnb_stake_tg"],
+      ["Ibg                 ", `<a href="ibg"           >Various</a>`,"iBG              ","https://farm.ibg.finance"]
     ]}
 
   let table = new AsciiTable().fromJSON(tableData);
