2bcb68d1582f8c8251120f998c44f0ca9d938e9f
Georgy Krasulya <gkrasulya@gmail.com>
2021-08-21 23:43:44 +0300

    add arbitrum warning

--- src/static/js/arbitrum.js
+++ src/static/js/arbitrum.js
@@ -4,11 +4,17 @@ $(function() {
 
 const main = async() => {
 
+  if (!ARBITRUM_LAUNCHED) {
+    _print('*** Arbitrum Mainnet is not live yet ***')
+    _print('*** Only whitelisted users can interact with dApps at the moment ***')
+    _print('')
+  }
+
   let tableData = {
     "title":"Arbitrum Network",
     "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
     "rows": [
-      ["GMX                 ", `<a href="/arbitrum/gmx"      >Various</a>`,"ETH,GMX          ","https://gmx.financial"],
+      ["GMX                 ", `<a href="/arbitrum/gmx"      >Various</a>`,"ETH,Escrow GMX   ","https://gmx.financial"],
      ]
   }
 
