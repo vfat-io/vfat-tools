c5e3eb1f267c31f7acc1b1da6732d3ca439bf08f
PolygonMaster <ydj0617@gmail.com>
2021-08-25 03:06:21 +0900

    Update polygon.js

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -320,6 +320,8 @@ const main = async () => {
       ['Sunrise'             , `<a href="sunrise"         >Various</a>`, 'SUNRISE      ', 'N/A'],
       ['Arable'              , `<a href="arable"          >Various</a>`, 'ARABELLA     ', 'https://arable.finance'],
       ['Ton Finance         ', `<a href="tomfinance"      >Various</a>`, 'TOM          ', 'https://tomfinance.vip'],
+	  ['Nemo Finance         ', `<a href="nemofinance"      >Various</a>`, 'NEMO          ', 'https://nemofinance.top'],
+	  ['Dice Finance         ', `<a href="dicefinance"      >Various</a>`, 'DICE          ', 'https://dicefinance.pro'],
     ],
   }
 
