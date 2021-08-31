309c786158f60521bfea3d0b959d579ca9c3dba9
stanfinance <balloonfantom@gmail.com>
2021-08-31 20:26:00 +0530

    Add Eternal Finance

--- src/static/js/avax.js
+++ src/static/js/avax.js
@@ -30,7 +30,8 @@ const main = async() => {
         ["DutchFinance",`<a href="dutch"      >Various</a>`,"DUTCH    ","https://avax.theflyingdutchman.finance" ],
         ["Xdollar     ",`<a href="xdollar"    >Various</a>`,"XDO      ","https://xdollar-avax.mcn.ventures" ],
         ["CrabFinance ",`<a href="crab   "    >Various</a>`,"CRAB     ","https://crabfinance.live" ],
-        ["Eskimo      ",`<a href="eskimo"     >Various</a>`,"ESKI     ","https://eskimo.farm" ]
+        ["Eskimo      ",`<a href="eskimo"     >Various</a>`,"ESKI     ","https://eskimo.farm" ],
+        ["EternalFinance" ,`<a href="ikaris"  >Various</a>`,"IKARIS   ","https://avax.eternalfinance.live" ],
     ]
   }
 
