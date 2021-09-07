547079b9470e50bc6d9027e66d6f9999cec14955
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-09-07 11:45:32 +0300

    liquid updated

--- src/static/js/fantom.js
+++ src/static/js/fantom.js
@@ -63,7 +63,7 @@ const main = async() => {
         "https://fantom.beefy.finance"
       ],
       [
-        "Liquid Driver",
+        "Liquid Driver *",
         `<a href='liquid'>Various</a>`,
         "LQDR",
         "https://www.liquiddriver.finance"
@@ -99,7 +99,7 @@ const main = async() => {
         "https://waka.finance"
       ],
       [
-        "Elk *",
+        "Elk",
         `<a href='elk'>Various</a>`,
         "ELK",
         "https://app.elk.finance"
