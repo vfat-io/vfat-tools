5fd222d1fe871a10296e2e58fcb615dae6ce611f
singularfarm <singularfarm@gmail.com>
2021-09-16 03:40:05 +0900

    add avax_singular

--- src/static/js/avax.js
+++ src/static/js/avax.js
@@ -47,7 +47,8 @@ const main = async() => {
         ["SmitheryFarms-Silver" ,`<a href="silver"  >Various</a>`,"SILVER","https://smitheryfarms.world" ],
         ["Eleven Finance" ,`<a href="eleven"  >Various</a>`,"ELE","https://eleven.finance" ],
         ["OmegaFarms   ",`<a href="omega  "  >Various</a>`,"OMEGA     ","https://avax.omegadefi.space" ],
-        ["Mammoth Finance  ",`<a href="mammoth  "  >Various</a>`,"TUSK     ","https://mammothfinance.io" ]
+        ["Mammoth Finance  ",`<a href="mammoth  "  >Various</a>`,"TUSK     ","https://mammothfinance.io" ],
+        ["Singular  ",`<a href="singular">Various</a>`,"SING", 'https://singular.farm/', ]
     ]
   }
 
