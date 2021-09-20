3879f81b0f45c3f7064c5a55407031b2349123f4
snowwhitedev <snowwhite91924@gmail.com>
2021-09-21 04:00:08 +0700

    feat: added unore staking pool

--- src/static/js/bsc.js
+++ src/static/js/bsc.js
@@ -514,7 +514,8 @@ const main = async() => {
 	    ["Meow                ", `<a href="meow"          >Various</a>`,"MEOWTH           ","https://meowswap.net"],
       ["PearZap             ", `<a href="pearzap"       >Various</a>`,"PEAR             ","https://pearzap.com"],
       ["Centaurus           ", `<a href="centaurus"     >Various</a>`,"CEN              ","https://cen.centaurus.network"],
-      ["SecureSwap          ", `<a href="secureswap"    >Various</a>`,"SCX              ","https://secureswap.io"]
+      ["SecureSwap          ", `<a href="secureswap"    >Various</a>`,"SCX              ","https://secureswap.io"],
+      ["UnoRe               ", `<a href="unore"         >Various</a>`,"UNORE            ","https://app.unore.io"]
     ]}
 
   let table = new AsciiTable().fromJSON(tableData);
