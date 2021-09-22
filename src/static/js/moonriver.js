897c4f8ccaa4e2295b6071210026edc2fc6d02a6
stanfinance <inNoKyc@protonmail.com>
2021-09-22 18:58:38 +0530

    add Moonengine

--- src/static/js/moonriver.js
+++ src/static/js/moonriver.js
@@ -10,7 +10,8 @@ const main = async() => {
     "rows": [
       ["Moonfarm                 ", `<a href="moonfarm"      >Various</a>`,"MOON   ","https://app.moonfarm.in"],
       ["Solarbeam                ", `<a href="solarbeam"     >Various</a>`,"SOLAR  ","https://solarbeam.io"],
-      ["Moonkafe                ", `<a href="moonkafe"     >Various</a>`,"KAFE  ","https://moon.kafe.finance"]
+      ["Moonkafe                ", `<a href="moonkafe"     >Various</a>`,"KAFE  ","https://moon.kafe.finance"],
+      ["MoonEngine              ", `<a href="moonengine"     >Various</a>`,"MOONENGINE  ","https://www.moonengine.info"],
      ]
   }
 
