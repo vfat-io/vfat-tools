2aee90b36aa8f2e0eee93304b35de770791b54a2
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-09-23 11:06:50 +0300

    Added dragonarbi, Closes #1759

--- src/static/js/arbitrum.js
+++ src/static/js/arbitrum.js
@@ -10,7 +10,7 @@ const main = async() => {
     "rows": [
       ["GMX                 ",""                                                                                               , `<a href="/arbitrum/gmx"         >Various</a>`,"ETH,Escrowed GMX   ","https://gmx.financial"],
       ["Sushi               ",""                                                                                               , `<a href="/arbitrum/sushi"       >Various</a>`,"SUSHI              ","https://app.sushi.com"],
-      ["DODO *              ",""                                                                                               , `<a href="/arbitrum/dodo"        >Various</a>`,"DODO               ","https://app.dodoex.io"],
+      ["DODO                ",""                                                                                               , `<a href="/arbitrum/dodo"        >Various</a>`,"DODO               ","https://app.dodoex.io"],
       ["NYAN                ",""                                                                                               , `<a href="/arbitrum/nyan"        >Various</a>`,"NYAN               ","https://arbinyan.xyz"],
       ["TheHoneyPot         ",""                                                                                               , `<a href="/arbitrum/honeypot"    >Various</a>`,"HONEY              ","https://thehoneypot.finance"],
       ["ArbiFarm            ",""                                                                                               , `<a href="/arbitrum/arbifarm"    >Various</a>`,"AFARM              ","https://www.arbifarm.fi"],
@@ -26,9 +26,10 @@ const main = async() => {
       ["Impermax Finance    ",""                                                                                               , `<a href="/arbitrum/impermax"    >Various</a>`,"IMX                ","https://arbitrum.impermax.finance"],
       ["Xdollar             ",""                                                                                               , `<a href="/arbitrum/xdollar"     >Various</a>`,"XDO                ","https://xdollar-arb.mcn.ventures" ],
       ["ArbiDolphin         ",""                                                                                               , `<a href="/arbitrum/arbidolphin" >Various</a>`,"ARBIDOLPHIN        ","https://arb.dolphinswap.finance"],
-      ["WhalesFinance       ",""                                                                                         , `<a href="/arbitrum/whalesfinance">Various</a>`,"WHALES            ","https://app.whalesfinance.io/"],
+      ["WhalesFinance       ",""                                                                                               , `<a href="/arbitrum/whalesfinance">Various</a>`,"WHALES            ","https://app.whalesfinance.io/"],
       ["Parrotdefi          ",`<a href="https://twitter.com/MCNVentures/status/1440498902688358402?s=20" target="_blank" class="mcn-reviewed">REVIEW</a>`, `<a href="/arbitrum/parrotdefi"  >Various</a>`,"PPEGG              ","https://parrotdefi.com"],
-      ["Arbis               ",""                                                                                               , `<a href="/arbitrum/arbis"       >Various</a>`,"Various            ","https://arbis.finance"]
+      ["Arbis               ",""                                                                                               , `<a href="/arbitrum/arbis"       >Various</a>`,"Various            ","https://arbis.finance"],
+      ["DragonArbi          ",""                                                                                               , `<a href="/arbitrum/dragonarbi"  >Various</a>`,"DRAGON             ","https://dragonarbi.com"]
      ]
   }
 
