a6bff7dd4819cb18094ab474120ceb83bfafe3ec
mrjunying98 <82926228+mrjunying98@users.noreply.github.com>
2021-09-26 15:52:03 +0800

    byield

--- src/static/js/bsc.js
+++ src/static/js/bsc.js
@@ -521,7 +521,8 @@ const main = async() => {
       ["1001 Nights Iago    ", `<a href="iago"          >Various</a>`,"IAGO             ","https://iago.1001nights.gold"],
       ["CC Swap             ", `<a href="ccswap"        >Various</a>`,"CC               ","https://app.ccswap.org"],
       ["UnoRe               ", `<a href="unore"         >Various</a>`,"UNORE            ","https://app.unore.io"],
-      ["Centaurus Net       ", `<a href="centaurus_net" >Various</a>`,"CEN              ","https://centaurus.network"]
+      ["Centaurus Net       ", `<a href="centaurus_net" >Various</a>`,"CEN              ","https://centaurus.network"],
+      ["BYield Finance      ", `<a href="byield"        >Various</a>`,"BYIELD           ","https://www.byieldfinance.com"]
     ]}
 
   let table = new AsciiTable().fromJSON(tableData);
