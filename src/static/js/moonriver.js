8cb5b82d6da7e8016a04e0b561e699d495582246
Dimosthenis Tsatsos <36194185+Dimosgr@users.noreply.github.com>
2021-09-27 14:09:02 +0300

    Update moonriver.js

--- src/static/js/moonriver.js
+++ src/static/js/moonriver.js
@@ -10,12 +10,12 @@ const main = async() => {
     "rows": [
       ["Moonfarm                 ", `<a href="moonfarm"      >Various</a>`,"MOON   ","https://app.moonfarm.in"],
       ["Solarbeam                ", `<a href="solarbeam"     >Various</a>`,"SOLAR  ","https://solarbeam.io"],
-      ["Moonkafe                ", `<a href="moonkafe"     >Various</a>`,"KAFE  ","https://moon.kafe.finance"],
-      ["MoonEngine              ", `<a href="moonengine"     >Various</a>`,"MOONENGINE  ","https://www.moonengine.info"],
-      ["Bcharity              ", `<a href="bcharity"     >Various</a>`,"GIVE  ","https://moongive.netlify.app/"],
-      ["MoonLake              ", `<a href="moonlake"     >Various</a>`,"MOONLAKE  ","https://moonlake.finance/"],
-      ["1Swap              ", `<a href="1swap"     >Various</a>`,"1SWAP  ","https://1swap.fi/"],
-      ["FoxyFinance             ", `<a href="foxy"       >Various</a>`,"FOXY  ","https://moon.foxyfinance.co"],
+      ["Moonkafe                 ", `<a href="moonkafe"     >Various</a>`,"KAFE  ","https://moon.kafe.finance"],
+      ["MoonEngine               ", `<a href="moonengine"     >Various</a>`,"MOONENGINE  ","https://www.moonengine.info"],
+      ["Bcharity                 ", `<a href="bcharity"     >Various</a>`,"GIVE  ","https://moongive.netlify.app"],
+      ["MoonLake                 ", `<a href="moonlake"     >Various</a>`,"MOONLAKE  ","https://moonlake.finance"],
+      ["FoxyFinance              ", `<a href="foxy"       >Various</a>`,"FOXY  ","https://moon.foxyfinance.co"],
+      ["1Swap                    ", `<a href="1swap"     >Various</a>`,"1SWAP  ","https://1swap.fi"]
     ]
   }
 
