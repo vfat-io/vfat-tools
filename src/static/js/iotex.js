cfa8570c1d808e607f25356e38ccedd5a441ebb3
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-10-07 16:11:53 +0300

    Added parrotdefi on iotex

--- src/static/js/iotex.js
+++ src/static/js/iotex.js
@@ -6,13 +6,14 @@ const main = async() => {
 
   let tableData = {
     "title":"IoTeX Network",
-    "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
+    "heading":["Pool Provider","MCN","LP", "Reward Tokens", "INFO"],
     "rows": [
-        ["Cyclone             ", `<a href="cyclone"       >Various</a>`,"CYC             ","https://cyclone.xyz"],
-        ["Magicland           ", `<a href="magicland"     >Various</a>`,"MAGIC           ","https://magicland.fi"],
-        ["ZoomSwap           ", `<a href="zoomswap"     >Various</a>`,"ZOOM           ","https://zoomswap.io"],
-        ["IotxCloudSwapFinance", `<a href="cloudswap"     >Various</a>`,"CLOUD           ","https://iotxcloudswap.finance"],
-        ["Seagull Finance     ", `<a href="seagull"     >Various</a>`,"FISH             ","https://seagull.finance"]
+        ["Cyclone             ","", `<a href="cyclone"       >Various</a>`,"CYC              ","https://cyclone.xyz"],
+        ["Magicland           ","", `<a href="magicland"     >Various</a>`,"MAGIC            ","https://magicland.fi"],
+        ["ZoomSwap            ","", `<a href="zoomswap"      >Various</a>`,"ZOOM             ","https://zoomswap.io"],
+        ["IotxCloudSwapFinance","", `<a href="cloudswap"     >Various</a>`,"CLOUD            ","https://iotxcloudswap.finance"],
+        ["Seagull Finance     ","", `<a href="seagull"       >Various</a>`,"FISH             ","https://seagull.finance"],
+        ["Parrotdefi          ",`<a href="https://twitter.com/MCNVentures/status/1440498902688358402?s=20" target="_blank" class="mcn-reviewed">REVIEW</a>`, `<a href="parrotdefi"  >Various</a>`,"PPEGG              ","https://parrotdefi.com"],
 
         ]
     }
