de0e8c458f62943eca008dde7ea70e2a1e8daee1
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-10-07 20:53:14 +0300

    Reviewed projects added up on pages

--- src/static/js/iotex.js
+++ src/static/js/iotex.js
@@ -8,12 +8,12 @@ const main = async() => {
     "title":"IoTeX Network",
     "heading":["Pool Provider","MCN","LP", "Reward Tokens", "INFO"],
     "rows": [
+        ["Parrotdefi          ",`<a href="https://twitter.com/MCNVentures/status/1440498902688358402?s=20" target="_blank" class="mcn-reviewed">REVIEW</a>`, `<a href="parrotdefi"  >Various</a>`,"iPEGG              ","https://iotex.parrotdefi.com"],
         ["Cyclone             ","", `<a href="cyclone"       >Various</a>`,"CYC              ","https://cyclone.xyz"],
         ["Magicland           ","", `<a href="magicland"     >Various</a>`,"MAGIC            ","https://magicland.fi"],
         ["ZoomSwap            ","", `<a href="zoomswap"      >Various</a>`,"ZOOM             ","https://zoomswap.io"],
         ["IotxCloudSwapFinance","", `<a href="cloudswap"     >Various</a>`,"CLOUD            ","https://iotxcloudswap.finance"],
         ["Seagull Finance     ","", `<a href="seagull"       >Various</a>`,"FISH             ","https://seagull.finance"],
-        ["Parrotdefi          ",`<a href="https://twitter.com/MCNVentures/status/1440498902688358402?s=20" target="_blank" class="mcn-reviewed">REVIEW</a>`, `<a href="parrotdefi"  >Various</a>`,"iPEGG              ","https://iotex.parrotdefi.com"],
 
         ]
     }
