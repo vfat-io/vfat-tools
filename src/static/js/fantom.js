4154bafced38698d590d16322f58684dcd44663d
secretemate <90825587+secretemate@users.noreply.github.com>
2021-11-05 21:49:21 +0200

    Update fantom.js

--- src/static/js/fantom.js
+++ src/static/js/fantom.js
@@ -106,7 +106,8 @@ const main = async() => {
       ["Mummyz Finance     ", `<a href="mummyz"       >Various</a>`,'zMUMMY       ',"https://mummyz.finance"],
       ['The Gas Station    ', `<a href="thegasstation">Various</a>`,'ftmGAS       ','https://gasstationcrypto.com'],
       ['COFFIN FINANCE     ', `<a href="coffin"       >Various</a>`,'COFFIN       ','https://app.coffin.finance'],
-      ['Babypig Finance    ', `<a href="babypig"      >Various</a>`,'fBABYPIG     ','https://babypig.finance']
+      ['Babypig Finance    ', `<a href="babypig"      >Various</a>`,'fBABYPIG     ','https://babypig.finance'],
+      ['Draco Force    ', `<a href="dracoforce"      >Various</a>`,'DRC     ','https://dracoforce.com']
     ]
   }
 
