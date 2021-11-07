a8ca20f0bf18654c0d820a1cb04d6320a354662d
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-11-07 13:17:06 +0200

    Added icecolony, Closes #1972

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -405,7 +405,8 @@ const main = async () => {
       ['PolyUnity Finance   ',"",  `<a href="polyunity"       >Various</a>`, 'UNITY        ', 'https://polyunity.finance'],
       ['Melon Finance       ',"",  `<a href="melonfinance"    >Various</a>`, 'MELON        ', 'https://melonfinance.cc'],
       ['Mushrooms Finance   ',"",  `<a href="mushrooms"       >Various</a>`, 'MM           ', 'https://mushrooms.finance/farms'],
-      ['Wheel Finance       ',"",  `<a href="wheelfinance"    >Various</a>`, 'WHEEL        ', 'https://wheelfinance.xyz']
+      ['Wheel Finance       ',"",  `<a href="wheelfinance"    >Various</a>`, 'WHEEL        ', 'https://wheelfinance.xyz'],
+      ['Icecolony           ',"",  `<a href="icecolony"       >Various</a>`, 'ICE          ', 'https://www.icecolony.com']
     ],
   }
 
