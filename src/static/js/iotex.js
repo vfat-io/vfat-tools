c383669a3082d85361f009253c978ddcc9e7a74c
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-11-09 16:36:24 +0200

    started minmax

--- src/static/js/iotex.js
+++ src/static/js/iotex.js
@@ -14,6 +14,8 @@ const main = async() => {
         ["ZoomSwap *          ","", `<a href="zoomswap"      >Various</a>`,"ZOOM             ","https://zoomswap.io"],
         ["IotxCloudSwapFinance","", `<a href="cloudswap"     >Various</a>`,"CLOUD            ","https://iotxcloudswap.finance"],
         ["Seagull Finance     ","", `<a href="seagull"       >Various</a>`,"FISH             ","https://seagull.finance"],
+        ["Seagull Finance     ","", `<a href="seagull"       >Various</a>`,"FISH             ","https://minmax.finance"],
+        ["MinMax              ","", `<a href="minmax"        >Various</a>`,"MAX / iMAGIC     ","https://minmax.finance"]
 
         ]
     }
