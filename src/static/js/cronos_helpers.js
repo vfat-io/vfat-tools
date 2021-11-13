be99d085a113e599d87c39bab10c1e9b25735994
Samuel Tay <samueltayishere@gmail.com>
2021-11-13 16:45:03 +0000

    Fix contract addresses
    Matched to https://github.com/crypto-org-chain/cronos-mainnet

--- src/static/js/cronos_helpers.js
+++ src/static/js/cronos_helpers.js
@@ -3,8 +3,8 @@ const CRONOS_VAULT_WANT_ABI = [{"inputs":[{"internalType":"contract IStrategy","
 
 const CronosTokens = [
   { "id": "crypto-com-coin", "symbol": "CRO", "contract": "0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23"},
-  { "id": "weth", "symbol": "WETH", "contract": "0x062E66477Faf219F25D27dCED647BF57C3107d52"},
-  { "id": "wrapped-bitcoin", "symbol": "WBTC", "contract": "0xe44Fd7fCb2b1581822D0c862B68222998a0c299a"},
+  { "id": "weth", "symbol": "WETH", "contract": "0xe44Fd7fCb2b1581822D0c862B68222998a0c299a"},
+  { "id": "wrapped-bitcoin", "symbol": "WBTC", "contract": "0x062E66477Faf219F25D27dCED647BF57C3107d52"},
   { "id": "usd-coin", "symbol": "USDC", "contract": "0xc21223249CA28397B4B6541dfFaEcC539BfF0c59"},
   { "id": "tether", "symbol": "USDT", "contract": "0x66e428c3f67a68878562e79A0234c1F83c208770"},
   { "id": "dai", "symbol": "DAI", "contract": "0xF2001B145b43032AAF5Ee2884e456CCd805F677D"}
