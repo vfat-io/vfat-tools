6e4aff6ae114923d68322700687cf834e4556fc1
WaterTim <a70546153@gmail.com>
2021-11-14 19:18:46 +0800

    add cronaswap

--- src/static/js/cronos.js
+++ src/static/js/cronos.js
@@ -9,7 +9,8 @@ const main = async() => {
     "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
     "rows": [
       ["CrodexSwap           ", `<a href='crodex'     >Various</a>`,"CRX          ","https://swap.crodex.app"],
-      ["VVS Finance          ", `<a href='vvs'        >Various</a>`,"VVS          ","https://vvs.finance"]
+      ["VVS Finance          ", `<a href='vvs'        >Various</a>`,"VVS          ","https://vvs.finance"],
+      ["CronaSwap            ", `<a href='crona'      >Various</a>`,"CRONA        ","https://app.cronaswap.org"]
     ]
   }
 
