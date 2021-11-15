c8052d7a2cb6b1d797a15b8c02d009d94665bf66
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-11-15 09:47:20 +0200

    Added krampus in index

--- src/static/js/fantom.js
+++ src/static/js/fantom.js
@@ -114,6 +114,7 @@ const main = async() => {
       ['UndeadFinance      ', `<a href="undeadfinance">Various</a>`,'UNDEAD       ','https://undeadfinance.com'],
       ['FanTerra           ', `<a href="fanterra"     >Various</a>`,'FANTERRA     ','https://app.fanterra.finance'],
       ['Bear Finance       ', `<a href="bear"         >Various</a>`,'BEAR         ','https://bearfinance.org'],
+      ['Krampus Finance    ', `<a href="krampus"      >Various</a>`,'KrampusToken ','https://krampus.finance']
     ]
   }
 
