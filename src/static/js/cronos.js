777e0ead459529555d41c0c97ac1d1e4d8a7ce69
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-11-18 11:09:49 +0200

    Added cronosyield

--- src/static/js/cronos.js
+++ src/static/js/cronos.js
@@ -12,7 +12,8 @@ const main = async() => {
       ["VVS Finance          ", `<a href='vvs'        >Various</a>`,"VVS          ","https://vvs.finance"],
       ["CroFarm              ", `<a href='crofarm'    >Various</a>`,"CRF          ","https://crofarm.app"],
       ["CronaSwap            ", `<a href='crona'      >Various</a>`,"CRONA        ","https://app.cronaswap.org"],
-      ["Crystl               ", `<a href='crystl'     >Various</a>`,"Various      ","https://cronos.crystl.finance"]
+      ["Crystl               ", `<a href='crystl'     >Various</a>`,"Various      ","https://cronos.crystl.finance"],
+      ["CronosYield          ", `<a href='cronosyield'>Various</a>`,"CPY          ","https://www.cronosyield.finance"]
     ]
   }
 
