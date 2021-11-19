cd59b449fdf1d26e8964704af3f302c19c296677
ZeusFinanceCRO <94494221+ZeusFinanceCRO@users.noreply.github.com>
2021-11-19 19:21:31 +0100

    Add files via upload

--- src/static/js/cronos.js
+++ src/static/js/cronos.js
@@ -15,7 +15,8 @@ const main = async() => {
       ["Crystl               ", `<a href='crystl'     >Various</a>`,"Various      ","https://cronos.crystl.finance"],
       ["CronosYield          ", `<a href='cronosyield'>Various</a>`,"CPY          ","https://www.cronosyield.finance"],
       ["StormSwap            ", `<a href='stormswap'  >Various</a>`,"WIND         ","https://cronos.stormswap.finance"],
-      ["Genesis Finance      ", `<a href='genesis'    >Various</a>`,"GENESIS      ","https://genesisfinance.app"]
+      ["Genesis Finance      ", `<a href='genesis'    >Various</a>`,"GENESIS      ","https://genesisfinance.app"],
+      ["ZeusFinance          ", `<a href='zeusfinance'>Various</a>`,"ZEUS         ","https://www.zeusfinance.app"]
     ]
   }
 
