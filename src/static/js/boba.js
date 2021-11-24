883161d2361e30cf019881d601dad77a4a47ce85
mildgiraffe <mildgiraffe@gmail.com>
2021-11-25 02:27:31 +0800

    add autofarm boba

--- src/static/js/boba.js
+++ src/static/js/boba.js
@@ -8,6 +8,7 @@ const main = async() => {
     "title":"Boba Network",
     "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
     "rows": [
+      ["Autofarm              ", `<a href='autofarm'      >Various</a>`,"Various      ","https://autofarm.network/boba"],
       ["Oolong Swap           ", `<a href='oolong'        >Various</a>`,"OLO          ","https://oolongswap.com"],
       ["Synapse               ", `<a href='synapse'       >Various</a>`,"SYN          ","https://synapseprotocol.com"],
       ["SwapperChan           ", `<a href='swapperchan'   >Various</a>`,"WAIFU        ","https://swapperchan.com"]
