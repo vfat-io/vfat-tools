3ae3f3d4a31b1c2b1c375c53adf9eb352ad76a13
Dimosthenis Tsatsos <36194185+Dimosgr@users.noreply.github.com>
2021-11-25 08:38:42 +0200

    Update boba.js

--- src/static/js/boba.js
+++ src/static/js/boba.js
@@ -8,10 +8,10 @@ const main = async() => {
     "title":"Boba Network",
     "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
     "rows": [
-      ["Autofarm              ", `<a href='autofarm'      >Various</a>`,"Various      ","https://autofarm.network/boba"],
       ["Oolong Swap           ", `<a href='oolong'        >Various</a>`,"OLO          ","https://oolongswap.com"],
       ["Synapse               ", `<a href='synapse'       >Various</a>`,"SYN          ","https://synapseprotocol.com"],
-      ["SwapperChan           ", `<a href='swapperchan'   >Various</a>`,"WAIFU        ","https://swapperchan.com"]
+      ["SwapperChan           ", `<a href='swapperchan'   >Various</a>`,"WAIFU        ","https://swapperchan.com"],
+      ["Autofarm              ", `<a href='autofarm'      >Various</a>`,"Various      ","https://autofarm.network/boba"]
     ]
   }
 
