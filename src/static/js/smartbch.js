a9a7f03e6c1b650a3a08c9dc1632b99e043fd502
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2021-12-09 17:12:01 +0200

    corrections in smartbch index

--- src/static/js/smartbch.js
+++ src/static/js/smartbch.js
@@ -8,10 +8,10 @@ const main = async() => {
     "title":"SmartBCH Network",
     "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
     "rows": [
-      ["BenSwap             ", `<a href="benswap"       >Various</a>`,"EBEN             ","https://benswap.cash\">https://benswap.cash"],
-      ["MistSwap            ", `<a href="mistswap"      >Various</a>`,"MIST, xMIST      ","https://app.mistswap.fi\">https://app.mistswap.fi"],
-      ["MuesliSwap BCH      ", `<a href="muesliswap"    >Various</a>`,"MILK             ","https://muesli.cash\">https://muesli.cash"],
-      ["TangoSwap           ", `<a href="tangoswap"     >Various</a>`,"TANGO            ","https://tangoswap.cash\">https://tangoswap.cash"]
+      ["BenSwap             ", `<a href="benswap"       >Various</a>`,"EBEN             ","https://benswap.cash"],
+      ["MistSwap            ", `<a href="mistswap"      >Various</a>`,"MIST, xMIST      ","https://app.mistswap.fi"],
+      ["MuesliSwap BCH      ", `<a href="muesliswap"    >Various</a>`,"MILK             ","https://muesli.cash"],
+      ["TangoSwap           ", `<a href="tangoswap"     >Various</a>`,"TANGO            ","https://tangoswap.cash"]
     ]}
 
   let table = new AsciiTable().fromJSON(tableData);
