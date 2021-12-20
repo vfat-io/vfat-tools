032e6c749f2a465f685f8d40a80c53986ffab140
AhegaoFinance <hello@ahegao.finance>
2021-12-20 21:53:52 +0000

    add ahegao.finance!

--- src/static/js/optimism.js
+++ src/static/js/optimism.js
@@ -10,7 +10,8 @@ const main = async() => {
     "rows": [
       ["Synapse               ", `<a href='synapse'    >Various</a>`,"SYN          ","https://synapseprotocol.com"],
       ["WePiggy               ", `<a href='wepiggy'    >Various</a>`,"WPC          ","https://app.wepiggy.com"],
-      ["NYAN                  ", `<a href="nyan"       >Various</a>`,"NYAN         ","https://optinyan.xyz/"]
+      ["NYAN                  ", `<a href="nyan"       >Various</a>`,"NYAN         ","https://optinyan.xyz/"],
+      ["ahegao.finance        ", `<a href="ahegao"     >Various</a>`,"AHEGAO       ","https://ahegao.finance"]
     ]
   }
 
