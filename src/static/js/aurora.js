d356250485d4329013261a1ea27bd55d6452704b
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2022-01-01 18:41:41 +0200

    Added auroraswap

--- src/static/js/aurora.js
+++ src/static/js/aurora.js
@@ -8,11 +8,12 @@ const main = async() => {
     "title":"Aurora Network",
     "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
     "rows": [
-        ["Trisolaris *",    `<a href="trisolaris"       >Various</a>`,      "TRI",       "https://www.trisolaris.io" ],
-        ["NearPad ",        `<a href="nearpad"          >Various</a>`,      "NearPad",   "https://nearpad.io" ],
-        ["WannaSwap ",      `<a href="wannaswap"        >Various</a>`,      "WANNA",     "https://wannaswap.finance" ],
-        ["Polaris",         `<a href="polaris"          >Various</a>`,      "PLRS",      "https://polaristoken.fi" ],
-        ["YellowSun",       `<a href="yellowsun"        >Various</a>`,      "YELLOWSUN", "https://aurora.yellowsun.live/" ],
+        ["Trisolaris",      `<a href="trisolaris"       >Various</a>`,      "TRI",          "https://www.trisolaris.io" ],
+        ["NearPad ",        `<a href="nearpad"          >Various</a>`,      "NearPad",      "https://nearpad.io" ],
+        ["WannaSwap ",      `<a href="wannaswap"        >Various</a>`,      "WANNA",        "https://wannaswap.finance" ],
+        ["Polaris",         `<a href="polaris"          >Various</a>`,      "PLRS",         "https://polaristoken.fi" ],
+        ["YellowSun",       `<a href="yellowsun"        >Various</a>`,      "YELLOWSUN",    "https://aurora.yellowsun.live" ],
+        ["AuroraSwap",      `<a href="auroraswap"       >Various</a>`,      "BRL",          "https://app.auroraswap.finance" ]
     ]
   }
 
