d91d556438256638e29c5f744cb1d29b6239650c
Zil-B <zilbthedegen@gmail.com>
2022-01-25 02:53:46 +0000

    Harmony: Add Comfy Money

--- src/static/js/harmony.js
+++ src/static/js/harmony.js
@@ -47,7 +47,8 @@ const main = async () => {
       ["Hundred",                "",`<a href='hundred'>Various</a>`,        "HND",           "https://hundred.finance"],
       ["Unite Finance",          "",`<a href='unite'>Various</a>`,          "USHARE",        "https://unitefinance.io"],
       ["WePiggy",                "",`<a href='wepiggy'>Various</a>`,        "WPC",           "https://app.wepiggy.com"],
-      ["Greedy Merchants Guild", "",`<a href='gmg'>Various</a>`,            "GMG",           "https://gmg.money"],
+        ["Greedy Merchants Guild", "", `<a href='gmg'>Various</a>`, "GMG", "https://gmg.money"],
+      ["Comfy Money", "",`<a href='comfymoney'>Various</a>`,            "COMFY & CSHARE",           "https://comfy.money"],
     ]
   }
 
