a414ccd135ec9ef09d099bd1d8f6fdd83fc87678
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2022-02-11 13:26:40 +0200

    Added 3om

--- src/static/js/fantom.js
+++ src/static/js/fantom.js
@@ -202,7 +202,8 @@ const main = async () => {
       ['Knights of Fantom  ', `<a href='knightsfantom'>Various</a>`, 'KNIGHTS      ', 'https://www.knights-of-fantom.com'],
       ['Based Finance      ', `<a href='based'        >Various</a>`, 'BASED        ', 'https://basedfinance.io'],
       ['MyMine             ', `<a href='mymine'       >Various</a>`, 'MYMINE       ', 'https://mymine.defiwaterfall.com'],
-      ['Platinum Finance   ', `<a href='platinumfi'   >Various</a>`, 'SPLAT / PLAT ', 'https://platinumfi.app']
+      ['Platinum Finance   ', `<a href='platinumfi'   >Various</a>`, 'SPLAT / PLAT ', 'https://platinumfi.app'],
+      ['3omb               ', `<a href='3omb'         >Various</a>`, '3SHARES / 3OMB', 'https://3omb.finance']
 
     ],
   }
