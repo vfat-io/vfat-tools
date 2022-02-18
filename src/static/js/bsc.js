2bf1bad6ce97e4de2726c108309e86b2ec832643
Steven Enamakel <enamakel@vezures.com>
2022-02-18 00:16:36 +0000

    added bsc and polygon network for arthcoin.com

--- src/static/js/bsc.js
+++ src/static/js/bsc.js
@@ -12,6 +12,7 @@ const main = async() => {
       ["Space               ",`<a href="https://twitter.com/MCNVentures/status/1442278437805170691?s=20" target="_blank" class="mcn-reviewed">REVIEW</a>`,                     `<a href="space"              >Various</a>`,"SPACE            ","https://farm.space"],
       ["Pancake             ","", `<a href="pancake"       >Various</a>`,"CAKE             ","https://pancakeswap.finance"],
       ["Apeswap             ","", `<a href="apeswap"       >Various</a>`,"BANANA           ","https://apeswap.finance"],
+      ["Arth                ","", `<a href="arth"          >Various</a>`,"ARTH             ","https://app.arthcoin.com"],
       ["Autofarm            ","", `<a href="autofarm"      >Various</a>`,"AUTOFARM         ","https://autofarm.network"],
       ["Kyber               ","", `<a href="kyber"         >Various</a>`,"KNC              ","https://dmm.exchange"],
       ["Bake                ","", `<a href="bake"          >Various</a>`,"BAKE             ","https://www.bakeryswap.org"],
