27e5fe85863d832b0e05a177c6963c5eef026bd7
vfat <info@vfat.tools>
2022-03-12 23:42:25 +0000

    rename maia to hermes, fix url

--- src/static/js/metis.js
+++ src/static/js/metis.js
@@ -8,11 +8,11 @@ $(function() {
       "title":"Metis Network",
       "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
       "rows": [
-        ["Tethys",          `<a href="tethys"         >Various</a>`,        "TETHYS",              "https://tethys.finance"],
-        ["Netswap",         `<a href="netswap"        >Various</a>`,        "NETT",                "https://netswap.io"],
-        ["Andro",           `<a href="andro"          >Various</a>`,        "ANDRO",               "https://androfinance.com"],
-        ["WOWswap",         `<a href="wowswap"        >Various</a>`,        "WOW",                 "https://wowswap.io"],
-        ["Maia DAO",        `<a href="maiadao"        >Various</a>`,        "HERMES",              "https://www.maiadao.xyz"]
+        ["Tethys",           `<a href="tethys"         >Various</a>`,        "TETHYS",              "https://tethys.finance"],
+        ["Netswap",          `<a href="netswap"        >Various</a>`,        "NETT",                "https://netswap.io"],
+        ["Andro",            `<a href="andro"          >Various</a>`,        "ANDRO",               "https://androfinance.com"],
+        ["WOWswap",          `<a href="wowswap"        >Various</a>`,        "WOW",                 "https://wowswap.io"],
+        ["Hermes (MaiaDAO)", `<a href="hermes"         >Various</a>`,        "HERMES",              "https://hermes.maiadao.io"]
       ]
     }
 
