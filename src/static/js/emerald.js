93a1b4f5aa2070911e48d6a62f029d5f13bce655
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2022-03-17 11:52:02 +0200

    Added yuzu

--- src/static/js/emerald.js
+++ src/static/js/emerald.js
@@ -13,7 +13,8 @@ const main = async() => {
       ["Lizard Exchange   ", `<a href='lizardexchange'>Various</a>`,"LIZ       ","https://lizard.exchange"],
       ["WePiggy           ", `<a href='wepiggy'     >Various</a>`,"WPC         ","https://app.wepiggy.com"],
       ["Valley Swap       ", `<a href='valleyswap'  >Various</a>`,"VS          ","https://valleyswap.com"],
-      ["GemKeeper Finance ", `<a href='gemkeeper'   >Various</a>`,"BLING       ","https://app.gemkeeper.finance"]
+      ["GemKeeper Finance ", `<a href='gemkeeper'   >Various</a>`,"BLING       ","https://app.gemkeeper.finance"],
+      ["Yuzu Swap         ", `<a href='yuzu'        >Various</a>`,"YUZU        ","https://app.yuzu-swap.com"]
 
     ]
   }
