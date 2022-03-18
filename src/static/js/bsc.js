415e4e2a1cdf220b9c4bf45075160f838e45f7a7
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2022-03-18 11:26:48 +0200

    Added clayswap, Closes #2270

--- src/static/js/bsc.js
+++ src/static/js/bsc.js
@@ -575,9 +575,10 @@ const main = async() => {
       ["Ygg                 ","", `<a href="ygg"           >Various</a>`,"YSHARE / YGG     ","https://www.ygg.money"],
       ["Arth                ","", `<a href="arth"          >Various</a>`,"ARTH             ","https://app.arthcoin.com"],
       ["Xray Swap           ","", `<a href="xrayswap"      >Various</a>`,"XRAY             ","https://xrayswap.com"],
-      ["Quartz.defi          ","", `<a href="quartzdefi"    >Various</a>`, "AMES / ASHARE  ","https://bsc-quartz-defi.app/"],
+      ["Quartz.defi         ","", `<a href="quartzdefi"    >Various</a>`, "AMES / ASHARE  ","https://bsc-quartz-defi.app/"],
       ["WOWswap             ","", `<a href="wowswap"       >Various</a>`,"WOW              ","https://wowswap.io"],
-      ["Atom Defi            ","",`<a href="atomdefi"      >Various</a>`,"ATOM             ","https://atomdefi.app"],
+      ["Atom Defi           ","",`<a href="atomdefi"       >Various</a>`,"ATOM             ","https://atomdefi.app"],
+      ["Clay Swap           ","",`<a href="clayswap"       >Various</a>`,"CLAY             ","https://clayswap.finance"]
     ]}
 
   let table = new AsciiTable().fromJSON(tableData);
