f2de34305bc38d9f17be65c89f20c1ca9a343fe5
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2022-03-25 15:37:08 +0200

    corrections in all index

--- src/static/js/all.js
+++ src/static/js/all.js
@@ -174,7 +174,7 @@ const main = async () => {
       ["WOWswap                      ", `<a href="../wowswap/"           >Various</a>`,"WOW                 ", "https://wowswap.io"],
       ["Aladdin (Concentrator)       ", `<a href="../concentrator/"      >Various</a>`,"aCRV                ", "https://concentrator.aladdin.club/#/vault"],
       ["Stargate                     ", `<a href="../stargate/"          >Various</a>`,"STG                 ", "https://stargate.finance"],
-      ["Stargate                     ", `<a href="../backd/"             >Various</a>`,"                    ", "https://backd.fund"]
+      ["Backd                        ", `<a href="../backd/"             >Various</a>`,"                    ", "https://backd.fund"]
     ]
   }
 
