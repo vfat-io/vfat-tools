aa2ba503b9e3e1b659e32e084842e26f0174a3ac
muh2772 <comethale772@gmail.com>
2022-04-10 21:23:34 +0530

    merging branches

--- src/static/js/polygon.js
+++ src/static/js/polygon.js
@@ -439,6 +439,7 @@ const main = async () => {
       ['Gains Network       ',"",  `<a href="gainsnetwork"    >Various</a>`, 'Various      ','https://gainsnetwork.io'],
       ['Hundred             ',"",  `<a href="hundred"         >Various</a>`, 'HND          ','https://hundred.finance'],
       ["Monox Finance       ","", `<a href="monox"         >Various</a>`,"vUNIT            ", "https://monox.finance"],
+      ['PolyDogeDAO         ',"",  `<a href="polydogedao"     >Various</a>`, 'PDSHARE / PDDOLLAR','https://polydogedao.net'],
     ],
   }
 
