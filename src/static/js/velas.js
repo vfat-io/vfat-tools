e434b1ce50b3c6a23c75508d577b138462650f35
Dimosthenis Tsatsos <dimos_athens_24@yahoo.gr>
2022-05-04 12:17:51 +0300

    Added centaurus on velas, Closes #2144

--- src/static/js/velas.js
+++ src/static/js/velas.js
@@ -9,7 +9,8 @@ const main = async() => {
     "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
     "rows": [
         ["WagyuSwap",     `<a href="wagyuswap"       >Various</a>`,      "WAG",       "https://exchange.wagyuswap.app" ],
-        ["JungleSwap",    `<a href="jungleswap"      >Various</a>`,      "JUNGLE",    "https://vlx.jungleswap.cash" ]
+        ["JungleSwap",    `<a href="jungleswap"      >Various</a>`,      "JUNGLE",    "https://vlx.jungleswap.cash" ],
+        ["Centaurus",     `<a href="centaurus"       >Various</a>`,      "CEN",       "https://cen.centaurus.network" ]
     ]
   }
 
