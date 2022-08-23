a13d97da8cb9194bb5ed2b5c06a87ff6da48a9ef
Dimosgr <dimos_athens_24@yahoo.gr>
2022-08-23 07:51:32 +0100

    Added dogepup

--- src/static/js/doge.js
+++ src/static/js/doge.js
@@ -11,7 +11,8 @@ const main = async() => {
         ["DogeCorn",          `<a href="dogecorn"       >Various</a>`,      "DOGECORN",          "https://dogecorntestapp.netlify.app"],
         ["DragonFruit",       `<a href="dragonfruit"    >Various</a>`,      "DFRUIT",            "https://dragonfruit.cash"],
         ["FruitsOfRyoshi",    `<a href="fruitsofryoshi" >Various</a>`,      "NONI",              "https://fruitsofryoshi.com"],
-        ["YodeSwap",          `<a href="yodeswap"       >Various</a>`,      "YODE",              "https://yodeswap.dog"]
+        ["YodeSwap",          `<a href="yodeswap"       >Various</a>`,      "YODE",              "https://yodeswap.dog"],
+        ["DogePup",           `<a href="dogepup"        >Various</a>`,      "DOGEPUP",           "https://dogepup.dog"]
     ]
   }
 
