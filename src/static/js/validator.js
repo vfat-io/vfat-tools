246e712e7a5fccd1045abe16fab3f9b11b612ddc
Dimosgr <dimos_athens_24@yahoo.gr>
2022-09-26 08:09:59 +0100

    Added performance

--- src/static/js/validator.js
+++ src/static/js/validator.js
@@ -7,7 +7,6 @@ async function main() {
     const App = await init_ethers();
 
     _print("Details of your Validators")
-    _print("");
 
     const _validatorsData = await $.ajax({
       url  : `https://beaconcha.in/api/v1/validator/eth1/${App.YOUR_ADDRESS}`,
@@ -33,7 +32,7 @@ async function main() {
     if(ownedValidators.length <= 0){
       _print("You do not own any Validators")
     }else{
-      _print(`You owned ${ownedValidators.length} Validators\n`);
+      _print(`You own ${ownedValidators.length} Validators\n`);
 
       const ValidatorsToString = ownedValidators.toString();
       const validatorsParameter = ValidatorsToString.replaceAll(',', '%2C');
@@ -43,30 +42,57 @@ async function main() {
         type : `GET`
       });
 
-      const ValidatorsBalanceDetails = await $.ajax({
+      const validatorsBalanceDetails = await $.ajax({
         url  : `https://beaconcha.in/api/v1/validator/${validatorsParameter}/balancehistory`,
         type : `GET`
       });
 
+      const validatorsPerformance = await $.ajax({
+        url  : `https://beaconcha.in/api/v1/validator/${validatorsParameter}/performance`,
+        type : `GET`
+      });
+
       if(validatorsDetails.data.status){
-        const balance = ValidatorsBalanceDetails.data[0].balance / 1e9;
-        const startingBalance = ValidatorsBalanceDetails.data[0].effectivebalance / 1e9;
-        //const state = validatorsDetails.data.status == "active_online" ? "Online" : "Offline";
+        const balance = validatorsBalanceDetails.data[0].balance / 1e9;
+        const startingBalance = validatorsBalanceDetails.data[0].effectivebalance / 1e9;
         const state = validatorsDetails.data.status;
+        const day = validatorsPerformance.data.performance1d / 1e9;
+        const month = validatorsPerformance.data.performance31d / 1e9;
+        const year = validatorsPerformance.data.performance365d / 1e9;
+        const yesterdayBalance = balance - day;
+        const oneMonthBeforeBalance = balance - month;
+        const oneYearBeforeBalance = balance - year;
+        const dailyAPR = day / yesterdayBalance * 100;
+        const monthlyAPR = month / oneMonthBeforeBalance * 100;
+        const yearlyAPR = year / oneYearBeforeBalance * 100;
         _print_bold(`Validator : ${validatorsDetails.data.validatorindex}`);
-        _print_bold(`State : ${state}`);
-        _print_bold(`Your Staking Balance : ${startingBalance}`);
-        _print_bold(`Your Balance : ${balance}`);
+        _print(`State : ${state}`);
+        _print(`Your Staking Balance : ${startingBalance}`);
+        _print(`Your Balance : ${balance}`);
+        _print(`Performance: 1 Day ${day} ETH (${dailyAPR.toFixed(4)}%)`);
+        _print(`Performance: 1 Month ${month} ETH (${monthlyAPR.toFixed(4)}%)`);
+        _print(`Performance: 1 Year ${year} ETH (${yearlyAPR.toFixed(4)}%)`);
       }else{
         for(let i = 0; i < validatorsDetails.data.length; i++){
-          const balance = ValidatorsBalanceDetails.data[i].balance / 1e9;
-          const startingBalance = ValidatorsBalanceDetails.data[i].effectivebalance / 1e9;
-          //const state = validatorsDetails.data[i].status == "active_online" ? "Online" : "Offline";
+          const balance = validatorsBalanceDetails.data[i].balance / 1e9;
+          const startingBalance = validatorsBalanceDetails.data[i].effectivebalance / 1e9;
           const state = validatorsDetails.data[i].status;
+          const day = validatorsPerformance.data[i].performance1d / 1e9;
+          const month = validatorsPerformance.data[i].performance31d / 1e9;
+          const year = validatorsPerformance.data[i].performance365d / 1e9;
+          const yesterdayBalance = balance - day;
+          const oneMonthBeforeBalance = balance - month;
+          const oneYearBeforeBalance = balance - year;
+          const dailyAPR = day / yesterdayBalance * 100;
+          const monthlyAPR = month / oneMonthBeforeBalance * 100;
+          const yearlyAPR = year / oneYearBeforeBalance * 100;
           _print_bold(`Validator : ${validatorsDetails.data[i].validatorindex}`);
-          _print_bold(`State : ${state}`);
-          _print_bold(`Your Staking Balance : ${startingBalance}`);
-          _print_bold(`Your Balance : ${balance}`);
+          _print(`State : ${state}`);
+          _print(`Your Staking Balance : ${startingBalance}`);
+          _print(`Your Balance : ${balance}`);
+          _print(`Performance: 1 Day ${day} ETH (${dailyAPR.toFixed(4)}%)`);
+          _print(`Performance: 1 Month ${month} ETH (${monthlyAPR.toFixed(4)}%)`);
+          _print(`Performance: 1 Year ${year} ETH (${yearlyAPR.toFixed(4)}%)`);
           _print("-------------------------------");
           _print("");
         }
@@ -76,7 +102,7 @@ async function main() {
     _print("");
 
     //adding a button for searching Validators
-    _print("Please add up to 100 Validators separated by a comma");
+    _print("Please add up to 100 Validators separated by a comma.");
 
     let input = document.createElement("input");
     input.setAttribute("id", "chosedValidators"); //with id chosedValidators i can get the values of the input
@@ -96,33 +122,60 @@ async function main() {
         type : `GET`
       });
   
-      const ValidatorsBalanceDetails = await $.ajax({
+      const validatorsBalanceDetails = await $.ajax({
         url  : `https://beaconcha.in/api/v1/validator/${validatorsParameter}/balancehistory`,
         type : `GET`
       });
 
-      if(validatorsDetails.data == null || ValidatorsBalanceDetails.data == null){
+      const validatorsPerformance = await $.ajax({
+        url  : `https://beaconcha.in/api/v1/validator/${validatorsParameter}/performance`,
+        type : `GET`
+      });
+
+      if(validatorsDetails.data == null || validatorsBalanceDetails.data == null){
         _print(validatorsDetails.status);
       }else{
         if(validatorsDetails.data.status){
-          const balance = ValidatorsBalanceDetails.data[0].balance / 1e9;
-          const startingBalance = ValidatorsBalanceDetails.data[0].effectivebalance / 1e9;
-          //const state = validatorsDetails.data.status == "active_online" ? "Online" : "Offline";
+          const balance = validatorsBalanceDetails.data[0].balance / 1e9;
+          const startingBalance = validatorsBalanceDetails.data[0].effectivebalance / 1e9;
           const state = validatorsDetails.data.status;
+          const day = validatorsPerformance.data.performance1d / 1e9;
+          const month = validatorsPerformance.data.performance31d / 1e9;
+          const year = validatorsPerformance.data.performance365d / 1e9;
+          const yesterdayBalance = balance - day;
+          const oneMonthBeforeBalance = balance - month;
+          const oneYearBeforeBalance = balance - year;
+          const dailyAPR = day / yesterdayBalance * 100;
+          const monthlyAPR = month / oneMonthBeforeBalance * 100;
+          const yearlyAPR = year / oneYearBeforeBalance * 100;
           _print_bold(`Validator : ${validatorsDetails.data.validatorindex}`);
-          _print_bold(`State : ${state}`);
-          _print_bold(`Your Staking Balance : ${startingBalance}`);
-          _print_bold(`Your Balance : ${balance}`);
+          _print(`State : ${state}`);
+          _print(`Your Staking Balance : ${startingBalance}`);
+          _print(`Your Balance : ${balance}`);
+          _print(`Performance: 1 Day ${day} ETH (${dailyAPR.toFixed(4)}%)`);
+          _print(`Performance: 1 Month ${month} ETH (${monthlyAPR.toFixed(4)}%)`);
+          _print(`Performance: 1 Year ${year} ETH (${yearlyAPR.toFixed(4)}%)`);
         }else{
           for(let i = 0; i < validatorsDetails.data.length; i++){
-            const balance = ValidatorsBalanceDetails.data[i].balance / 1e9;
-            const startingBalance = ValidatorsBalanceDetails.data[i].effectivebalance / 1e9;
-            //const state = validatorsDetails.data[i].status == "active_online" ? "Online" : "Offline";
+            const balance = validatorsBalanceDetails.data[i].balance / 1e9;
+            const startingBalance = validatorsBalanceDetails.data[i].effectivebalance / 1e9;
             const state = validatorsDetails.data[i].status;
+            const day = validatorsPerformance.data[i].performance1d / 1e9;
+            const month = validatorsPerformance.data[i].performance31d / 1e9;
+            const year = validatorsPerformance.data[i].performance365d / 1e9;
+            const yesterdayBalance = balance - day;
+            const oneMonthBeforeBalance = balance - month;
+            const oneYearBeforeBalance = balance - year;
+            const dailyAPR = day / yesterdayBalance * 100;
+            const monthlyAPR = month / oneMonthBeforeBalance * 100;
+            const yearlyAPR = year / oneYearBeforeBalance * 100;
             _print_bold(`Validator : ${validatorsDetails.data[i].validatorindex}`);
-            _print_bold(`State : ${state}`);
-            _print_bold(`Your Staking Balance : ${startingBalance}`);
-            _print_bold(`Your Balance : ${balance}`);
+            _print(`State : ${state}`);
+            _print(`Your Staking Balance : ${startingBalance}`);
+            _print(`Your Balance : ${balance}`);
+            _print(`Performance: 1 Day ${day} ETH (${dailyAPR.toFixed(4)}%)`);
+            _print(`Performance: 1 Month ${month} ETH (${monthlyAPR.toFixed(4)}%)`);
+            _print(`Performance: 1 Year ${year} ETH (${yearlyAPR.toFixed(4)}%)`);
             _print("-------------------------------");
             _print("");
           }
