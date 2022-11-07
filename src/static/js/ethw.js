0d6fdc93ede53e0109b91ec100a92cce9a698f4c
Dimosgr <dimos_athens_24@yahoo.gr>
2022-11-07 12:11:34 +0000

    lfgswap started

--- src/static/js/ethw.js
+++ src/static/js/ethw.js
@@ -8,7 +8,7 @@ const main = async () => {
     heading: ["Pool Provider","MCN", "LP", "Reward Tokens", "INFO"],
     rows: [
       ['Uniwswap           ',"", `<a href="uniwswap"            >Various</a>`,'UNIW         ','https://farm.uniwswap.com'],
-      ['LFG Finance        ',"", `<a href="lfg"                 >Various</a>`,'LFG          ','https://app.lfgswap.finance']
+      //['LFG Finance        ',"", `<a href="lfg"                 >Various</a>`,'LFG          ','https://app.lfgswap.finance']
     ],
   }
 
