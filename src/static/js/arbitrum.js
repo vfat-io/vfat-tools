8711fc88b7c64c31f6a507321af84b7d2dd50fb9
Dimosthenis Tsatsos <36194185+Dimosgr@users.noreply.github.com>
2023-01-14 06:34:25 +0000

    Update arbitrum.js

--- src/static/js/arbitrum.js
+++ src/static/js/arbitrum.js
@@ -58,7 +58,7 @@ const main = async() => {
       ["Waterfall Arbitrum  ",""                                                                                               , `<a href="/arbitrum/waterfall"   >Various</a>`,"WATERFALL          ","https://www.defiwaterfall.com"],
       ["Oreo Swap           ",""                                                                                               , `<a href="/arbitrum/oreo"        >Various</a>`,"OREO               ","https://oreoswap.finance"],
       ["Sugar Finance       ",""                                                                                               , `<a href="/arbitrum/sugar"       >Various</a>`,"SUGAR              ","https://sugarfinance.io"],
-      ["Waterfall DEX  ",""                                                                                               , `<a href="/arbitrum/wtfdex"   >Various</a>`,"WTFX          ","https://exchange.wtfdex.com/#/swap"]  
+      ["Waterfall DEX       ",""                                                                                               , `<a href="/arbitrum/wtfdex"      >Various</a>`,"WTFX               ","https://exchange.wtfdex.com/#/swap"]  
     ]
   }
 
