fceb2c5f3e638db0731d087c18669e2c769d0753
Dimosgr <dimos_athens_24@yahoo.gr>
2023-04-30 13:25:01 +0100

    Added gnd

--- src/static/js/arbitrum_gnd.js
+++ src/static/js/arbitrum_gnd.js
@@ -13,15 +13,214 @@ async function main() {
 
    const GND_CHEF_ADDR = "0xf6a9F64350AeeBA91fd42c5434Af8208C3367b53";
    const rewardTokenTicker = "GND";
+   const rewardXGNDTokenTicker = "XGND";
    const GND_CHEF = new ethers.Contract(GND_CHEF_ADDR, GND_CHEF_ABI, App.provider);
 
    const rewardsPerWeek = await GND_CHEF.GNDPerSecond() /1e18 * 604800;
+   const rewardsXGNDPerWeek = await GND_CHEF.xGNDPerSecond() /1e18 * 604800;
 
     const tokens = {};
     const prices = await getArbitrumPrices();
 
-    await loadArbitrumChefContract(App, tokens, prices, GND_CHEF, GND_CHEF_ADDR, GND_CHEF_ABI, rewardTokenTicker,
-        "GND", null, rewardsPerWeek, "pendingGND", [1]);
+    await loadGndArbitrumChefContract(App, tokens, prices, GND_CHEF, GND_CHEF_ADDR, GND_CHEF_ABI, rewardTokenTicker,
+        "GND", null, rewardsPerWeek, "pendingGND", [1], rewardXGNDTokenTicker, "xGND", rewardsXGNDPerWeek, "pendingxGND");
 
     hideLoading();
   }
+
+async function loadGndArbitrumChefContract(App, tokens, prices, chef, chefAddress, chefAbi, rewardTokenTicker,
+  rewardTokenFunction, rewardsPerBlockFunction, rewardsPerWeekFixed, pendingRewardsFunction,
+  deathPoolIndices, rewardXGNDTokenTicker, rewardXGNDTokenFunction, rewardsXGNDPerWeek, pendingXGNDRewardsFunction) {
+  const chefContract = chef ?? new ethers.Contract(chefAddress, chefAbi, App.provider);
+
+  const poolCount = parseInt(await chefContract.poolLength(), 10);
+  const totalAllocPoints = await chefContract.totalAllocPoint();
+
+  _print(`<a href='https://arbiscan.io/address/${chefAddress}' target='_blank'>Staking Contract</a>`);
+  _print(`Found ${poolCount} pools.\n`)
+
+  _print(`Showing incentivized pools only.\n`);
+
+  const rewardTokenAddress = await chefContract.callStatic[rewardTokenFunction]();
+  const rewardXGNDTokenAddress = await chefContract.callStatic[rewardXGNDTokenFunction]();
+  const rewardToken = await getArbitrumToken(App, rewardTokenAddress, chefAddress);
+  const rewardXGNDToken = await getArbitrumToken(App, rewardXGNDTokenAddress, chefAddress);
+  const rewardsPerWeek = rewardsPerWeekFixed ??
+    await chefContract.callStatic[rewardsPerBlockFunction]()
+    / 10 ** rewardToken.decimals * 604800 / 13.5
+
+  const poolInfos = await Promise.all([...Array(poolCount).keys()].map(async (x) =>
+    await getGndArbitrumPoolInfo(App, chefContract, chefAddress, x, pendingRewardsFunction, pendingXGNDRewardsFunction)));
+
+  var tokenAddresses = [].concat.apply([], poolInfos.filter(x => x.poolToken).map(x => x.poolToken.tokens));
+
+  await Promise.all(tokenAddresses.map(async (address) => {
+    tokens[address] = await getArbitrumToken(App, address, chefAddress);
+  }));
+
+  if (deathPoolIndices) {   //load prices for the deathpool assets
+    deathPoolIndices.map(i => poolInfos[i])
+      .map(poolInfo =>
+        poolInfo.poolToken ? getPoolPrices(tokens, prices, poolInfo.poolToken, "arbitrum") : undefined);
+  }
+
+  const poolPrices = poolInfos.map(poolInfo => poolInfo.poolToken ? getPoolPrices(tokens, prices, poolInfo.poolToken, "arbitrum") : undefined);
+
+
+  _print("Finished reading smart contracts.\n");
+
+  let aprs = []
+  for (i = 0; i < poolCount; i++) {
+    if (poolPrices[i]) {
+      const apr = printGndArbitrumChefPool(App, chefAbi, chefAddress, prices, tokens, poolInfos[i], i, poolPrices[i],
+        totalAllocPoints, rewardsPerWeek, rewardTokenTicker, rewardTokenAddress,
+        pendingRewardsFunction, null,  "arbitrum", poolInfos[i].depositFee, poolInfos[i].withdrawFee,
+        rewardsXGNDPerWeek, rewardXGNDTokenTicker, rewardXGNDTokenAddress, pendingXGNDRewardsFunction)
+      aprs.push(apr);
+    }
+  }
+  let totalUserStaked = 0, totalStaked = 0, averageApr = 0;
+  for (const a of aprs) {
+    if (!isNaN(a.totalStakedUsd)) {
+      totalStaked += a.totalStakedUsd;
+    }
+    if (a.userStakedUsd > 0) {
+      totalUserStaked += a.userStakedUsd;
+      averageApr += a.userStakedUsd * a.yearlyAPR / 100;
+    }
+  }
+  averageApr = averageApr / totalUserStaked;
+  _print_bold(`Total Staked: $${formatMoney(totalStaked)}`);
+  if (totalUserStaked > 0) {
+    _print_bold(`\nYou are staking a total of $${formatMoney(totalUserStaked)} at an average APR of ${(averageApr * 100).toFixed(2)}%`)
+    _print(`Estimated earnings:`
+      + ` Day $${formatMoney(totalUserStaked * averageApr / 365)}`
+      + ` Week $${formatMoney(totalUserStaked * averageApr / 52)}`
+      + ` Year $${formatMoney(totalUserStaked * averageApr)}\n`);
+  }
+  return { prices, totalUserStaked, totalStaked, averageApr }
+}
+
+function printGndArbitrumChefPool(App, chefAbi, chefAddr, prices, tokens, poolInfo, poolIndex, poolPrices,
+  totalAllocPoints, rewardsPerWeek, rewardTokenTicker, rewardTokenAddress,
+  pendingRewardsFunction, fixedDecimals, chain = "eth", depositFee = 0, withdrawFee = 0,
+  rewardsXGNDPerWeek, rewardXGNDTokenTicker, rewardXGNDTokenAddress, pendingXGNDRewardsFunction) {
+  fixedDecimals = fixedDecimals ?? 2;
+  const sp = (poolInfo.stakedToken == null) ? null : getPoolPrices(tokens, prices, poolInfo.stakedToken, chain);
+  var poolRewardsPerWeek = poolInfo.allocPoints / totalAllocPoints * rewardsPerWeek;
+  var poolXGNDRewardsPerWeek = poolInfo.allocPoints / totalAllocPoints * rewardsXGNDPerWeek;
+  if (poolRewardsPerWeek == 0 && rewardsPerWeek != 0) return;
+  const userStaked = poolInfo.userLPStaked ?? poolInfo.userStaked;
+  const rewardPrice = getParameterCaseInsensitive(prices, rewardTokenAddress)?.usd;
+  const rewardXGNDPrice = getParameterCaseInsensitive(prices, rewardXGNDTokenAddress)?.usd;
+  const staked_tvl = sp?.staked_tvl ?? poolPrices.staked_tvl;
+  _print_inline(`${poolIndex} - `);
+  poolPrices.print_price(chain);
+  sp?.print_price(chain);
+  const apr = printGndAPR(rewardTokenTicker, rewardXGNDTokenTicker, rewardPrice, rewardXGNDPrice, poolRewardsPerWeek, poolXGNDRewardsPerWeek, poolPrices.stakeTokenTicker,
+    staked_tvl, userStaked, poolPrices.price, fixedDecimals);
+  if (poolInfo.userLPStaked > 0) sp?.print_contained_price(userStaked);
+  if (poolInfo.userStaked > 0) poolPrices.print_contained_price(userStaked);
+  printGndArbitrumChefContractLinks(App, chefAbi, chefAddr, poolIndex, poolInfo.address, pendingRewardsFunction,
+    pendingXGNDRewardsFunction, rewardTokenTicker, rewardXGNDTokenTicker, poolPrices.stakeTokenTicker, poolInfo.poolToken.unstaked,
+    poolInfo.userStaked, poolInfo.pendingRewardTokens, poolInfo.pendingXGNDRewardTokens, fixedDecimals, rewardPrice, rewardXGNDPrice, chain, depositFee, withdrawFee);
+  return apr;
+}
+
+function printGndArbitrumChefContractLinks(App, chefAbi, chefAddr, poolIndex, poolAddress, pendingRewardsFunction, pendingXGNDRewardsFunction,
+  rewardTokenTicker, rewardXGNDTokenTicker, stakeTokenTicker, unstaked, userStaked, pendingRewardTokens, pendingXGNDRewardTokens, fixedDecimals,
+  rewardTokenPrice, rewardXGNDPrice, chain, depositFee, withdrawFee) {
+  fixedDecimals = fixedDecimals ?? 2;
+  const approveAndStake = async function () {
+    return chefArbitrumContract_stake(chefAbi, chefAddr, poolIndex, poolAddress, App)
+  }
+  const unstake = async function () {
+    return chefArbitrumContract_unstake(chefAbi, chefAddr, poolIndex, App, pendingRewardsFunction)
+  }
+  const claim = async function () {
+    return chefArbitrumContract_claim(chefAbi, chefAddr, poolIndex, App, pendingRewardsFunction, claimFunction)
+  }
+  if (depositFee > 0) {
+    _print_link(`Stake ${unstaked.toFixed(fixedDecimals)} ${stakeTokenTicker} - Fee ${depositFee}%`, approveAndStake)
+  } else {
+    _print_link(`Stake ${unstaked.toFixed(fixedDecimals)} ${stakeTokenTicker}`, approveAndStake)
+  }
+  if (withdrawFee > 0) {
+    _print_link(`Unstake ${userStaked.toFixed(fixedDecimals)} ${stakeTokenTicker} - Fee ${withdrawFee}%`, unstake)
+  } else {
+    _print_link(`Unstake ${userStaked.toFixed(fixedDecimals)} ${stakeTokenTicker}`, unstake)
+  }
+  _print_link(`Claim ${pendingRewardTokens.toFixed(fixedDecimals)} ${rewardTokenTicker} ($${formatMoney(pendingRewardTokens * rewardTokenPrice)}) + ${pendingXGNDRewardTokens.toFixed(fixedDecimals)} ${rewardXGNDTokenTicker} ($${formatMoney(pendingXGNDRewardTokens * rewardXGNDPrice)})`, claim)
+  _print(`Staking or unstaking also claims rewards.`)
+  _print("");
+}
+
+function printGndAPR(rewardTokenTicker, rewardXGNDTokenTicker, rewardPrice, rewardXGNDPrice, poolRewardsPerWeek, poolXGNDRewardsPerWeek,
+                  stakeTokenTicker, staked_tvl, userStaked, poolTokenPrice,
+                  fixedDecimals) {
+  var usdPerWeek = poolRewardsPerWeek * rewardPrice;
+  var usdXGNDPerWeek = poolXGNDRewardsPerWeek * rewardXGNDPrice;
+  fixedDecimals = fixedDecimals ?? 2;
+  _print(`${rewardTokenTicker} Per Week: ${poolRewardsPerWeek.toFixed(fixedDecimals)} ($${formatMoney(usdPerWeek)})`);
+  _print(`${rewardXGNDTokenTicker} Per Week: ${poolXGNDRewardsPerWeek.toFixed(fixedDecimals)} ($${formatMoney(usdXGNDPerWeek)})`);
+  var weeklyAPR = usdPerWeek / staked_tvl * 100;
+  var dailyAPR = weeklyAPR / 7;
+  var yearlyAPR = weeklyAPR * 52;
+
+  var weeklyXGNDAPR = usdXGNDPerWeek / staked_tvl * 100;
+  var dailyXGNDAPR = weeklyXGNDAPR / 7;
+  var yearlyXGNDAPR = weeklyXGNDAPR * 52;
+  _print(`APR: Day ${dailyAPR.toFixed(2)}% Week ${weeklyAPR.toFixed(2)}% Year ${yearlyAPR.toFixed(2)}%`);
+  _print(`APR: Day ${dailyXGNDAPR.toFixed(2)}% Week ${weeklyXGNDAPR.toFixed(2)}% Year ${yearlyXGNDAPR.toFixed(2)}%`);
+  var userStakedUsd = userStaked * poolTokenPrice;
+  var userStakedPct = userStakedUsd / staked_tvl * 100;
+  _print(`You are staking ${userStaked.toFixed(fixedDecimals)} ${stakeTokenTicker} ($${formatMoney(userStakedUsd)}), ${userStakedPct.toFixed(2)}% of the pool.`);
+  var userWeeklyRewards = userStakedPct * poolRewardsPerWeek / 100;
+  var userDailyRewards = userWeeklyRewards / 7;
+  var userYearlyRewards = userWeeklyRewards * 52;
+
+  var userXGNDWeeklyRewards = userStakedPct * poolXGNDRewardsPerWeek / 100;
+  var userXGNDDailyRewards = userXGNDWeeklyRewards / 7;
+  var userXGNDYearlyRewards = userXGNDWeeklyRewards * 52;
+  if (userStaked > 0) {
+    _print(`Estimated ${rewardTokenTicker} earnings:`
+        + ` Day ${userDailyRewards.toFixed(fixedDecimals)} ($${formatMoney(userDailyRewards*rewardPrice)}) + ${userXGNDDailyRewards.toFixed(fixedDecimals)} ($${formatMoney(userXGNDDailyRewards*rewardXGNDPrice)})`
+        + ` Week ${userWeeklyRewards.toFixed(fixedDecimals)} ($${formatMoney(userWeeklyRewards*rewardPrice)}) + ${userXGNDWeeklyRewards.toFixed(fixedDecimals)} ($${formatMoney(userXGNDWeeklyRewards*rewardXGNDPrice)})`
+        + ` Year ${userYearlyRewards.toFixed(fixedDecimals)} ($${formatMoney(userYearlyRewards*rewardPrice)}) + ${userXGNDYearlyRewards.toFixed(fixedDecimals)} ($${formatMoney(userXGNDYearlyRewards*rewardXGNDPrice)})`);
+  }
+  return {
+    userStakedUsd,
+    totalStakedUsd : staked_tvl,
+    userStakedPct,
+    yearlyAPR: yearlyAPR + yearlyXGNDAPR,
+    userYearlyUsd : userYearlyRewards * rewardPrice
+  }
+}
+
+async function getGndArbitrumPoolInfo(app, chefContract, chefAddress, poolIndex, pendingRewardsFunction, pendingXGNDRewardsFunction) {
+  const poolInfo = await chefContract.poolInfo(poolIndex);
+  if (poolInfo.allocPoint == 0) {
+    return {
+      address: poolInfo.lpToken,
+      allocPoints: poolInfo.allocPoint ?? 1,
+      poolToken: null,
+      userStaked: 0,
+      pendingRewardTokens: 0,
+    };
+  }
+  const poolToken = await getArbitrumToken(app, poolInfo.lpToken ?? poolInfo.token ?? poolInfo.stakingToken, chefAddress);
+  const userInfo = await chefContract.userInfo(poolIndex, app.YOUR_ADDRESS);
+  const pendingRewardTokens = await chefContract.callStatic[pendingRewardsFunction](poolIndex, app.YOUR_ADDRESS);
+  const pendingXGNDRewardTokens = await chefContract.callStatic[pendingXGNDRewardsFunction](poolIndex, app.YOUR_ADDRESS);
+  const staked = userInfo.amount / 10 ** poolToken.decimals;
+  return {
+    address: poolInfo.lpToken ?? poolInfo.token ?? poolInfo.stakingToken,
+    allocPoints: poolInfo.allocPoint ?? 1,
+    poolToken: poolToken,
+    userStaked: staked,
+    pendingRewardTokens: pendingRewardTokens / 10 ** 18,
+    pendingXGNDRewardTokens: pendingXGNDRewardTokens / 10 ** 18,
+    depositFee: (poolInfo.depositFeeBP ?? 0) / 100,
+    withdrawFee: (poolInfo.withdrawFeeBP ?? 0) / 100
+  };
+}
