00687f43b7383bfd68251fadd57554e9756c195c
Dimosgr <dimos_athens_24@yahoo.gr>
2023-06-02 17:36:01 +0300

    Added some more logic

--- src/static/js/optimism_fire.js
+++ src/static/js/optimism_fire.js
@@ -23,17 +23,60 @@ async function main() {
 
 async function loadXFire(App, prices){
 
+  let eligible = false;
+  function reqListener() {
+    const resp = this.responseText;
+    const _data = resp.split();
+    const data = _data[0].split('\n');
+    eligible = data.includes(App.YOUR_ADDRESS);
+    console.log(eligible);
+  }
+
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "https://raw.githubusercontent.com/ltj866/airdrop-0/main/addresses", false);
+  req.send();
+
   const FIRE_ADDR = "";
   const MAIN_CONTRACT_ADDR = "";
 
-  const approveAndStake = async function() {
-    return contract_stake(MAIN_CONTRACT_ADDR, App, FIRE_ADDR)
+  const FIRE_CONTRACT = "";
+  const MAIN_CONTRACT = "";
+
+  const startTime = await MAIN_CONTRACT.startTime();  // / 1000???
+  const endTime = await MAIN_CONTRACT.endTime();  // / 1000???
+  const multiplier = await MAIN_CONTRACT.multiplier();
+  const wFireAmount = await MAIN_CONTRACT.userInfo(App.YOUR_ADDRESS);
+  const fireAmount = await FIRE_CONTRACT.balanceOf(App.YOUR_ADDRESS);
+
+  const startDate = new Date(startTime);  //??? .toLocaleString()???
+  const endDate = new Date(endTime);  //??? .toLocaleString()???
+
+  _print(`Starting date : ${startDate}\n`);
+  _print(`Ending date : ${endDate}\n`);
+
+  if(eligible){
+    _print("You are a part of the airdrop\n");
+
+    const approveAndStake = async function() {
+      return contract_stake(MAIN_CONTRACT_ADDR, App, FIRE_ADDR)
+    }
+    const unstake = async function() {
+      return contract_unstake(MAIN_CONTRACT_ADDR, App)
+    }
+    if(Date.now() / 1000 < startTime){
+      _print_link(`Stake ${totalOwnedFire.toFixed(2)} ${stakeTicker}`, approveAndStake);
+    }else{
+      _print("Deposit period has closed");
+    }
+    if(Date.now() / 1000 > endTime){
+      _print_link(`Unstake ${totalOwnedXFire.toFixed(2)} ${rewardTicker}`, unstake);
+    }else{
+      _print(`You can withdraw you FIRE after ${endDate}`);
+    }
+  }else{
+    _print("You are not a part of the airdrop");
   }
-  const unstake = async function() {
-    return contract_unstake(MAIN_CONTRACT_ADDR, App)
-  }
-  _print_link(`Stake ${totalOwnedFire.toFixed(2)} ${stakeTicker}`, approveAndStake)
-  _print_link(`Unstake ${totalOwnedXFire.toFixed(2)} ${rewardTicker}`, unstake)
 }
 
 const contract_stake = async function(contractAddress, App, stakeTokenAddr) {
