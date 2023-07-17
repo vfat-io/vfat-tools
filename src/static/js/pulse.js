8fac45b20798c8548176585d7ee35262d48633cf
ColaFactory <colafactory.dev@gmail.com>
2023-07-17 04:32:13 +0200

    listing Colafactory

--- src/static/js/pulse.js
+++ src/static/js/pulse.js
@@ -10,6 +10,7 @@ const main = async() => {
     "rows": [
       ["Pulsex                 ", `<a href="pulsex"      >Various</a>`,"INC               ","https://app.pulsex.com"],
       ["Daytona Finance        ", `<a href="daytona"     >Various</a>`,"TONI              ","https://www.daytona.finance"],
+      ["ColaFactory            ", `<a href="colaFactory" >Various</a>`,"COLAFACTORY       ","https://colafactory.com/"]
       ["Velocimeter            ", `<a href="velocimeter" >Various</a>`,"FLOW              ","https://pulse.velocimeter.xyz"],
       ["Pulserate              ", `<a href="pulserate"   >Various</a>`,"PSHARE / PRATE    ","https://pulserate.io"],
       ["YieldPulse Finance     ", `<a href="yieldx"      >Various</a>`,"YIELDX            ","https://yieldpulse.finance/"]
