66a4f5791856ea90e1776d4808aa93aa4534bebc
Dimosthenis Tsatsos <36194185+Dimosgr@users.noreply.github.com>
2023-07-30 16:34:57 +0300

    Update pulse.js

--- src/static/js/pulse.js
+++ src/static/js/pulse.js
@@ -8,7 +8,6 @@ const main = async() => {
     "title":"Pulse Network",
     "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
     "rows": [
-      ["Yogurtfinance          ", `<a href="yogurtfinance" >Various</a>`,"YOGURTFINANCE   ","https://yogurtfinance.com/"],
       ["Pulsex                 ", `<a href="pulsex"      >Various</a>`,"INC               ","https://app.pulsex.com"],
       ["Daytona Finance        ", `<a href="daytona"     >Various</a>`,"TONI              ","https://www.daytona.finance"],
       ["Velocimeter            ", `<a href="velocimeter" >Various</a>`,"FLOW              ","https://pulse.velocimeter.xyz"],
@@ -16,7 +15,8 @@ const main = async() => {
       ["YieldPulse Finance     ", `<a href="yieldx"      >Various</a>`,"YIELDX            ","https://yieldpulse.finance"],
       ["ColaFactory            ", `<a href="colaFactory" >Various</a>`,"COLAFACTORY       ","https://colafactory.com"],
       ["Chirp Finance          ", `<a href="chirp"       >Various</a>`,"CHIRP             ","https://chirp.finance"],
-      ["Pulseaxe               ", `<a href="pulseaxe"    >Various</a>`,"CHOP / AXE        ","https://pulseaxe.com"]
+      ["Pulseaxe               ", `<a href="pulseaxe"    >Various</a>`,"CHOP / AXE        ","https://pulseaxe.com"],
+      ["Yogurtfinance          ", `<a href="yogurtfinance" >Various</a>`,"YOGURTFINANCE   ","https://yogurtfinance.com"]
 
     ].reverse()
   }
