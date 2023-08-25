4f71872c9f57bbe651ef98dac850f38e0b82ac05
Dimosgr <dimos_athens_24@yahoo.gr>
2023-08-25 17:32:40 +0300

    removed adamant from the index page

--- src/static/js/arbitrum.js
+++ src/static/js/arbitrum.js
@@ -86,7 +86,7 @@ const main = async() => {
       ["Wombex Finance      ", `<a href="/arbitrum/wombex"      >Various</a>`,"WOM                ","https://wombex.finance"],
       ["Fire                ", `<a href="/arbitrum/fire"        >Various</a>`,"wFIRE              ",""],
       ["Spartadex           ", `<a href="/arbitrum/sparta"      >Various</a>`,"SPARTA             ","https://app.spartadex.io"],
-      ["Adamant Finance     ", `<a href="/arbitrum/adamant"     >Various</a>`,"ARBY / ETH         ","https://adamant.finance"]
+      //["Adamant Finance     ", `<a href="/arbitrum/adamant"     >Various</a>`,"ARBY / ETH         ","https://adamant.finance"]
     ].reverse()
   }
 
