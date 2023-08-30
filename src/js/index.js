92dd066539f4937c864a7e617d610d0340035c25
Dimosgr <dimos_athens_24@yahoo.gr>
2023-08-30 12:52:26 +0300

    corrections in alienbase

--- src/js/index.js
+++ src/js/index.js
@@ -150,7 +150,7 @@ window.NETWORKS = {
       "decimals": 18
     },
     "rpcUrls": [
-      "https://base.meowrpc.com"
+      "https://base.publicnode.com"
     ],
     "blockExplorerUrls": [
       "https://basescan.org"
