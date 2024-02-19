9d74bf5120c6076f9de719d57e6535bbbe48275e
Dimosgr <dimos_athens_24@yahoo.gr>
2024-02-19 09:33:23 +0000

    Added to main page list

--- src/static/js/pages.js
+++ src/static/js/pages.js
@@ -202,7 +202,7 @@ const main = async () => {
       ["Prisma Finance *             ", `<a href="prisma/"            >Various</a>`, "PRISMA             ", "https://app.prismafinance.com"],
       ["9inch                        ", `<a href="9inch/"             >Various</a>`, "BBC                ", "https://app.9inch.io"],
       ["Morpheus                     ", `<a href="morpheus/"          >Various</a>`, "MOR                ", "https://mor.org"],
-      //["Ethena                       ", `<a href="ethena/"            >Various</a>`, "                   ", "https://app.ethena.fi"]
+      ["Ethena                       ", `<a href="ethena/"            >Various</a>`, "                   ", "https://app.ethena.fi"]
     ].reverse()
   }
 
