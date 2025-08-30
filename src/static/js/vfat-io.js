7580a338627821525022da7ada5cbd01a8c8a9a6
Dimosgr <dimos_athens_24@yahoo.gr>
2025-08-30 09:53:20 +0300

    Changes on ui and added more projects on vfat-io page

--- src/static/js/vfat-io.js
+++ src/static/js/vfat-io.js
@@ -8,10 +8,24 @@ const main = async() => {
     "title":"vfat.io Projects Map",
     "heading":["Project", "Type", "Link"],
     "rows": [
-      ["Aerodrome             ", "CL ", `<a href='/base/aerodrome'           >/base/aerodrome</a>`],
-      ["Blackhole             ", "CL ", `<a href='/avax/sickle'                 >/avax/sickle</a>`],
-      ["                      ", "V2 ", `<a href='/avax/blackhole'           >/avax/blackhole</a>`],
-      ["Velodrome             ", "CL ", `<a href='/base/aerodrome'       >/optimism/velodrome</a>`],
+      ["Aerodrome             ", "CL ", `<a href='/base/aerodrome'           >Base</a>`],
+      ["Blackhole             ", "CL ", `<a href='/avax/sickle'              >Avax</a>`],
+      ["Blackhole             ", "V2 ", `<a href='/avax/blackhole'           >Avax</a>`],
+      ["Etherex               ", "CL ", `<a href='/linea/sickle'             >Linea</a>`],
+      ["Etherex               ", "V2 ", `<a href='/linea/etherex'            >Linea</a>`],
+      ["Uniswap               ", "CL ", `<a href='/arbitrum/uniswap'         >Arbitrum</a>`],
+      ["Uniswap               ", "CL ", `<a href='/avax/uniswap'             >Avax</a>`],
+      ["Uniswap               ", "CL ", `<a href='/base/uniswap'             >Base</a>`],
+      ["Uniswap               ", "CL ", `<a href='/bnb/uniswap'              >Bnb</a>`],
+      ["Uniswap               ", "CL ", `<a href='/hemi/uniswap'             >Hemi</a>`],
+      ["Uniswap               ", "CL ", `<a href='/optimism/uniswap'         >Optimism</a>`],
+      ["Uniswap               ", "CL ", `<a href='/polygon/uniswap'          >Polygon</a>`],
+      ["Uniswap               ", "CL ", `<a href='/unichain/uniswap'         >Unichain</a>`],
+      ["Velodrome             ", "CL ", `<a href='/fraxtal/sickle'           >Fraxtal</a>`],
+      ["Velodrome             ", "CL ", `<a href='/ink/sickle'               >Ink</a>`],
+      ["Velodrome             ", "CL ", `<a href='/mode/sickle'              >Mode</a>`],
+      ["Velodrome             ", "CL ", `<a href='/optimism/sickle'          >Optimism</a>`],
+      ["Velodrome             ", "CL ", `<a href='/unichain/sickle'          >Unichain</a>`],
     ]
   }
 
